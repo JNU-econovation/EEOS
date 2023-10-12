e8701997beffb19ae51bd5a82d1542218701f96e
bada308 <203190@jnu.ac.kr>
2023-10-12 15:10:57 +0900

    fix : Input 컴포넌트 수정
    - autoComplete 옵션 off
    - onChange props 옵셔널로 수정

--- FE/components/common/Input.tsx
+++ FE/components/common/Input.tsx
@@ -1,5 +1,5 @@
 interface InputProps extends React.HTMLProps<HTMLInputElement> {
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const Input = ({ id, label, value, onChange, placeholder }: InputProps) => {
@@ -13,6 +13,7 @@ const Input = ({ id, label, value, onChange, placeholder }: InputProps) => {
         value={value}
         onChange={onChange}
         placeholder={placeholder}
+        autoComplete="off"
         className="border-2 border-gray-300 rounded-md px-3 py-2 shadow-sm focus:outline-none focus:border-tertiary w-full"
       />
     </div>
