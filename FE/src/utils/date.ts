a0eb8f7341d23087563e046de11f5e859bc44f13
bada308 <203190@jnu.ac.kr>
2023-10-12 15:02:40 +0900

    feat : 날짜 형식 type 추가
    - 기존의 형태를 default 더 짧은 형태를 short로 지정
    - type props 추가

--- FE/src/utils/date.ts
+++ FE/src/utils/date.ts
@@ -1,11 +1,16 @@
 const weekList = ["일", "월", "화", "수", "목", "금", "토"];
 
-export const convertDate = (timestamp: string) => {
+export const convertDate = (
+  timestamp: string,
+  type: "default" | "short" = "default"
+) => {
   const date = new Date(parseInt(timestamp));
   const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
   const dayOfWeek = weekList[date.getDay()];
 
-  return `${year}년 ${month}월 ${day}일 (${dayOfWeek})`;
+  return type === "default"
+    ? `${year}년 ${month}월 ${day}일 (${dayOfWeek})`
+    : `${year}-${month}-${day} (${dayOfWeek})`;
 };
