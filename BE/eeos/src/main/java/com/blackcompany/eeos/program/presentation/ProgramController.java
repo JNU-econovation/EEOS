cfc55cb2d2b9f53c63773224c0103e84c71a7395
kssumin <201566@jnu.ac.kr>
2023-10-15 18:15:38 +0900

    fix/be : 단 건 프로그램 가져오는 controller 구현

--- BE/eeos/src/main/java/com/blackcompany/eeos/program/presentation/ProgramController.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/program/presentation/ProgramController.java
@@ -5,10 +5,14 @@ import com.blackcompany.eeos.common.presentation.respnose.ApiResponseBody.Succes
 import com.blackcompany.eeos.common.presentation.respnose.ApiResponseGenerator;
 import com.blackcompany.eeos.common.presentation.respnose.MessageCode;
 import com.blackcompany.eeos.program.application.dto.CreateProgramRequest;
+import com.blackcompany.eeos.program.application.dto.GetProgramResponse;
 import com.blackcompany.eeos.program.application.usecase.CreateProgramUsecase;
+import com.blackcompany.eeos.program.application.usecase.GetProgramUsecase;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +24,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProgramController {
 
 	private final CreateProgramUsecase createBoardUsecase;
+	private final GetProgramUsecase getProgramUsecase;
 
 	@PostMapping
 	public ApiResponse<SuccessBody<Long>> create(@RequestBody @Valid CreateProgramRequest request) {
 		Long programId = createBoardUsecase.create(request);
 		return ApiResponseGenerator.success(programId, HttpStatus.CREATED, MessageCode.CREATE);
+	}
+
+	@GetMapping("/{eventId}")
+	public ApiResponse<SuccessBody<GetProgramResponse>> findOne(
+			@PathVariable("eventId") Long eventId) {
+		GetProgramResponse response = getProgramUsecase.getProgram(eventId);
+		return ApiResponseGenerator.success(response, HttpStatus.OK, MessageCode.GET);
 	}
 }
