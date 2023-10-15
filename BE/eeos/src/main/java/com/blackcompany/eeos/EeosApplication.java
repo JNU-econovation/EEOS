208bfc7ebfba4df273fb16499e91a2da1bd92c1a
kssumin <201566@jnu.ac.kr>
2023-10-15 16:17:51 +0900

    feat/#11/setting : 초기 개발환경 세팅

---
+++ BE/eeos/src/main/java/com/blackcompany/eeos/EeosApplication.java
@@ -0,0 +1,12 @@
+package com.blackcompany.eeos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeosApplication.class, args);
+	}
+}
