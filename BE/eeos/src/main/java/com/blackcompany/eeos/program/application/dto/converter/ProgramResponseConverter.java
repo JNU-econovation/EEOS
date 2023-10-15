c2cc0969a3bd9aac04a86dda82c9b87e30212a1b
kssumin <201566@jnu.ac.kr>
2023-10-15 18:15:13 +0900

    fix/be : status를 파라미터로 받도록 변경

--- BE/eeos/src/main/java/com/blackcompany/eeos/program/application/dto/converter/ProgramResponseConverter.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/program/application/dto/converter/ProgramResponseConverter.java
@@ -2,17 +2,19 @@ package com.blackcompany.eeos.program.application.dto.converter;
 
 import com.blackcompany.eeos.program.application.domain.ProgramModel;
 import com.blackcompany.eeos.program.application.dto.GetProgramResponse;
+import com.blackcompany.eeos.program.persistence.EventStatus;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProgramResponseConverter {
-	public GetProgramResponse from(ProgramModel target) {
+
+	public GetProgramResponse from(ProgramModel target, EventStatus status) {
 		return GetProgramResponse.builder()
 				.id(target.getId())
 				.title(target.getTitle())
 				.content(target.getContent())
 				.programDate(target.getProgramDate())
-				.eventStatus(target.getEventStatus())
+				.eventStatus(status.name())
 				.build();
 	}
 }
