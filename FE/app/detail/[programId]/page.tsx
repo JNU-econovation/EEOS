d0a8c592d5b4a822ae8cba602a6f2e11e321173d
bada308 <203190@jnu.ac.kr>
2023-10-16 22:14:28 +0900

    fix : 컴포넌트 이름 변경
    - EventInfo -> ProgramInfo
    
    Related #9

--- FE/app/detail/[programId]/page.tsx
+++ FE/app/detail/[programId]/page.tsx
@@ -1,5 +1,5 @@
-import EventInfo from "@/components/detail/EventInfo";
 import MemberList from "@/components/detail/MemberList";
+import ProgramInfo from "@/components/detail/ProgramInfo";
 import SubLayout from "@/components/layout/SubLayout";
 
 interface DetailPageProps {
@@ -12,7 +12,7 @@ const DetailPage = ({ params }: DetailPageProps) => {
   const { programId } = params;
   return (
     <SubLayout right="btn">
-      <EventInfo programId={programId} />
+      <ProgramInfo programId={programId} />
       <section className="w-screen bg-soft_secondary flex flex-col gap-10 items-center py-16">
         <MemberList programId={programId} attendStatus="attend" />
         <MemberList programId={programId} attendStatus="absent" />
