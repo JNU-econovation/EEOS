683dc354d98f76fa3242e7d21aa1186b50661c95
kssumin <201566@jnu.ac.kr>
2023-10-16 16:26:10 +0900

    feat/be : 프로그램 내용 수정 service 구현

--- BE/eeos/src/main/java/com/blackcompany/eeos/program/application/service/ProgramService.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/program/application/service/ProgramService.java
@@ -1,14 +1,16 @@
 package com.blackcompany.eeos.program.application.service;
 
 import com.blackcompany.eeos.program.application.domain.ProgramModel;
-import com.blackcompany.eeos.program.application.dto.AbstractProgramRequest;
+import com.blackcompany.eeos.program.application.dto.CommandProgramResponse;
 import com.blackcompany.eeos.program.application.dto.GetProgramResponse;
 import com.blackcompany.eeos.program.application.dto.converter.ProgramResponseConverter;
+import com.blackcompany.eeos.program.application.dto.suppport.AbstractProgramRequest;
 import com.blackcompany.eeos.program.application.exception.NotFoundProgramException;
 import com.blackcompany.eeos.program.application.model.converter.ProgramEntityConverter;
 import com.blackcompany.eeos.program.application.model.converter.ProgramRequestConverter;
 import com.blackcompany.eeos.program.application.usecase.CreateProgramUsecase;
 import com.blackcompany.eeos.program.application.usecase.GetProgramUsecase;
+import com.blackcompany.eeos.program.application.usecase.UpdateProgramUsecase;
 import com.blackcompany.eeos.program.persistence.ProgramEntity;
 import com.blackcompany.eeos.program.persistence.ProgramRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,19 +18,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ProgramService implements CreateProgramUsecase, GetProgramUsecase {
+public class ProgramService
+		implements CreateProgramUsecase, GetProgramUsecase, UpdateProgramUsecase {
 	private final ProgramRequestConverter requestConverter;
 	private final ProgramEntityConverter entityConverter;
 	private final ProgramResponseConverter responseConverter;
 	private final ProgramRepository programRepository;
 
 	@Override
-	public Long create(AbstractProgramRequest request) {
+	public CommandProgramResponse create(AbstractProgramRequest request) {
 		ProgramModel model = requestConverter.from(request);
 		ProgramEntity entity = entityConverter.toEntity(model);
 		ProgramEntity save = programRepository.save(entity);
 
-		return save.getId();
+		return responseConverter.from(save.getId());
 	}
 
 	@Override
@@ -40,5 +43,14 @@ public class ProgramService implements CreateProgramUsecase, GetProgramUsecase {
 
 		ProgramModel model = entityConverter.from(programEntity);
 		return responseConverter.from(model, model.calculateEventStatus());
+	}
+
+	@Override
+	public CommandProgramResponse update(Long programId, AbstractProgramRequest request) {
+		ProgramModel model = requestConverter.from(programId, request);
+		ProgramEntity entity = entityConverter.toEntity(model);
+		ProgramEntity updateEntity = programRepository.save(entity);
+
+		return responseConverter.from(updateEntity.getId());
 	}
 }
