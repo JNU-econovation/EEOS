9fe7b8d9350274ad70fb6b9fb1b69be68008b727
kssumin <201566@jnu.ac.kr>
2023-10-20 03:33:05 +0900

    feat/be : attendEntity converter 구현

---
+++ BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/model/converter/AttendEntityConverter.java
@@ -0,0 +1,32 @@
+package com.blackcompany.eeos.attend.application.model.converter;
+
+import com.blackcompany.eeos.attend.application.model.AttendModel;
+import com.blackcompany.eeos.attend.persistence.AttendEntity;
+import com.blackcompany.eeos.common.support.converter.AbstractEntityConverter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AttendEntityConverter implements AbstractEntityConverter<AttendEntity, AttendModel> {
+
+	@Override
+	public AttendModel from(AttendEntity source) {
+		return AttendModel.builder()
+				.id(source.getId())
+				.memberId(source.getMemberId())
+				.programId(source.getProgramId())
+				.build();
+	}
+
+	@Override
+	public AttendEntity toEntity(AttendModel source) {
+		return AttendEntity.builder()
+				.id(source.getId())
+				.memberId(source.getMemberId())
+				.programId(source.getProgramId())
+				.build();
+	}
+
+	public AttendEntity toEntity(Long memberId, Long programId) {
+		return AttendEntity.builder().memberId(memberId).programId(programId).build();
+	}
+}
