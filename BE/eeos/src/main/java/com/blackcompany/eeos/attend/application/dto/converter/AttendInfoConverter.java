85f06becf0e6ea8cc194ab93c551be28cf1585da
kssumin <201566@jnu.ac.kr>
2023-10-20 03:40:29 +0900

    feat/be : attendInfo response 구현

---
+++ BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/dto/converter/AttendInfoConverter.java
@@ -0,0 +1,18 @@
+package com.blackcompany.eeos.attend.application.dto.converter;
+
+import com.blackcompany.eeos.attend.application.dto.AttendInfoResponse;
+import com.blackcompany.eeos.member.application.model.MemberModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AttendInfoConverter {
+	public AttendInfoResponse from(final MemberModel target, final String attendStatus) {
+		return AttendInfoResponse
+				.builder()
+				.id(target.getId())
+				.generation(target.getGeneration())
+				.name(target.getName())
+				.attendStatus(attendStatus)
+				.build();
+	}
+}
