6de214135985e055daf16932f65f90cd4c15ed99
kssumin <201566@jnu.ac.kr>
2023-10-20 03:36:59 +0900

    feat/be : memberRepository 구현

---
+++ BE/eeos/src/main/java/com/blackcompany/eeos/member/persistence/MemberRepository.java
@@ -0,0 +1,5 @@
+package com.blackcompany.eeos.member.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {}
