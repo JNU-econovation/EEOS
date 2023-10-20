5a83c4c4f60c0e49758405b479e1444f057cb085
kssumin <201566@jnu.ac.kr>
2023-10-20 14:47:08 +0900

    feat/be : programId, memberId에 맞는 attend 가져오도록 repository 구현

--- BE/eeos/src/main/java/com/blackcompany/eeos/attend/persistence/AttendRepository.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/attend/persistence/AttendRepository.java
@@ -1,8 +1,11 @@
 package com.blackcompany.eeos.attend.persistence;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AttendRepository extends JpaRepository<AttendEntity, Long> {
 	List<AttendEntity> findAllByProgramId(Long programId);
+
+	Optional<AttendEntity> findByProgramIdAndMemberId(Long programId, Long memberId);
 }
