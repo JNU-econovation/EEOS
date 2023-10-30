2849b6ad193ef7194062127b1b7bbbf6265e3af3
kssumin <201566@jnu.ac.kr>
2023-10-30 22:32:06 +0900

    feat/be : 이전 상태가 동일하지 않을 때 발생하는 예외 구현

--- BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/dto/converter/AttendInfoConverter.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/dto/converter/AttendInfoConverter.java
@@ -1,17 +1,18 @@
 package com.blackcompany.eeos.attend.application.dto.converter;
 
 import com.blackcompany.eeos.attend.application.dto.AttendInfoResponse;
+import com.blackcompany.eeos.attend.application.model.AttendStatus;
 import com.blackcompany.eeos.member.application.model.MemberModel;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AttendInfoConverter {
-	public AttendInfoResponse from(final MemberModel target, final String attendStatus) {
+	public AttendInfoResponse from(final MemberModel target, final AttendStatus attendStatus) {
 		return AttendInfoResponse.builder()
 				.id(target.getId())
 				.generation(target.getGeneration())
 				.name(target.getName())
-				.attendStatus(attendStatus)
+				.attendStatus(attendStatus.getStatus())
 				.build();
 	}
 }
