35cf85d9d83d895de078e7783ce61a812f930c9c
bada308 <203190@jnu.ac.kr>
2023-11-02 21:01:56 +0900

    fix: endpoint 수정 Related #41

--- FE/src/constants/API.ts
+++ FE/src/constants/API.ts
@@ -1,10 +1,10 @@
 const PROGRAM = "/programs";
 
 const MEMBER = {
-  POST_ATTENDSTATUS: "/attend/programs",
+  UPDATE_ATTENDSTATUS: "/attend/programs",
   GET_MEMBER_LIST_BY_STATUS: (programId: number) =>
     `/programs/${programId}/members`,
-  GET_ALL_MEMBERS: "/attend/candidate/program",
+  GET_ALL_MEMBERS: "/attend/programs",
 };
 
 Object.freeze(MEMBER);
