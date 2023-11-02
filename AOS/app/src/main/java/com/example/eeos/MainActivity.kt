e25dff173ccd55f2f0377e902c84261dd13143f9
HyeonjiJang <jhg050908@gmail.com>
2023-11-02 13:39:00 +0900

    style/AOS: ktlint 스타일 변환 명령 실행

--- AOS/app/src/main/java/com/example/eeos/MainActivity.kt
+++ AOS/app/src/main/java/com/example/eeos/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -35,6 +36,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
