2a8e92cd26bbc25be61d531b7251a585b3fd89ba
kssumin <201566@jnu.ac.kr>
2023-11-04 13:56:09 +0900

    feat : 완료 여부에 따라 프로그램 조회 쿼리

--- BE/eeos/src/main/java/com/blackcompany/eeos/program/persistence/ProgramRepository.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/program/persistence/ProgramRepository.java
@@ -1,5 +1,17 @@
 package com.blackcompany.eeos.program.persistence;
 
+import java.sql.Timestamp;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ProgramRepository extends JpaRepository<ProgramEntity, Long> {}
+public interface ProgramRepository extends JpaRepository<ProgramEntity, Long> {
+
+	@Query("SELECT p FROM ProgramEntity p WHERE p.programDate < :now ORDER BY p.programDate DESC")
+	Page<ProgramEntity> findAllByEnd(@Param("now") Timestamp now, Pageable pageable);
+
+	@Query("SELECT p FROM ProgramEntity p WHERE p.programDate > :now ORDER BY p.programDate DESC")
+	Page<ProgramEntity> findAllByIng(@Param("now") Timestamp now, Pageable pageable);
+}
