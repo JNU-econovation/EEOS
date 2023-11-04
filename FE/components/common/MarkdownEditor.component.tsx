9ec3c014ca8275cf80981d41161e6afb14d1c7eb
bada308 <203190@jnu.ac.kr>
2023-11-05 04:57:30 +0900

    feat: 마크다운 라이브러리 교체 - from "react-md-editor" to "react-markdown" - list enter시 한 글자 더 써지는 문제 때문
    Related #61

--- FE/components/common/MarkdownEditor.component.tsx
+++ FE/components/common/MarkdownEditor.component.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import MDEditor from "@uiw/react-md-editor";
 import "./styles/markdown-editor.styles.css";
+import MarkdownViewer from "./MarkdownViewer.component";
 
 interface MarkdownEditorProps {
   id: string;
@@ -23,15 +23,28 @@ const MarkdownEditor = ({
       <label htmlFor={id} className="text-sm">
         {label}
       </label>
-      <MDEditor
-        id={id}
-        value={value}
-        onChange={onChange}
-        hideToolbar={true}
-        visiableDragbar={false}
-        height={400}
-        placeholder={placeholder}
-      />
+      <div className="grid h-96 w-full grid-cols-[1fr_0fr] rounded-md border border-gray-300 p-2 transition-transform lg:grid-cols-[1fr_1fr]">
+        <textarea
+          id={id}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          className="h-full resize-none overflow-y-scroll border-r border-gray-300 p-4 outline-none"
+          placeholder={placeholder}
+          onKeyDown={(e) => {
+            if (e.key === "Tab") {
+              e.preventDefault();
+              const start = e.currentTarget.selectionStart;
+              const end = e.currentTarget.selectionEnd;
+              const target = e.currentTarget;
+              const value = target.value;
+              target.value =
+                value.substring(0, start) + "  " + value.substring(end);
+              target.selectionStart = target.selectionEnd = start + 2;
+            }
+          }}
+        />
+        <MarkdownViewer value={value} className="hidden lg:block" />
+      </div>
     </div>
   );
 };
