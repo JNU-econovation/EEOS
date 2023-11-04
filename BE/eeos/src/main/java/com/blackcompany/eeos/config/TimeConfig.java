f50f4cdb5367782e607ec42f47dc114d7023abf3
kssumin <201566@jnu.ac.kr>
2023-11-04 10:17:24 +0900

    feat : timezone 설정

---
+++ BE/eeos/src/main/java/com/blackcompany/eeos/config/TimeConfig.java
@@ -0,0 +1,14 @@
+package com.blackcompany.eeos.config;
+
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TimeConfig {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+}
