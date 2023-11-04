ad281ac38ee81d7b00dfa1deb241e21e6c8800a9
kssumin <201566@jnu.ac.kr>
2023-11-04 10:19:43 +0900

    feat : index 설정

--- BE/eeos/resources/local-develop-environment/mysql-init.d/01_create_table.sql
+++ BE/eeos/resources/local-develop-environment/mysql-init.d/01_create_table.sql
@@ -7,18 +7,24 @@ create table program
     is_deleted      boolean      not null,
     updated_date    datetime     not null,
     program_content varchar(255) not null,
-    program_date    timestamp    not null,
+    program_date    TIMESTAMP    not null,
     program_title   varchar(255) not null,
     primary key (program_id)
 ) engine = InnoDB;
 
 create table member
 (
-    member_id    bigint       not null auto_increment,
-    created_date datetime     not null,
-    is_deleted   boolean      not null,
-    updated_date datetime     not null,
-    member_name  varchar(255) not null,
-    member_generation int not null,
+    member_id         bigint       not null auto_increment,
+    created_date      datetime     not null,
+    is_deleted        boolean      not null,
+    updated_date      datetime     not null,
+    member_name       varchar(255) not null,
+    member_generation BIGINT       not null,
     primary key (member_id)
 ) engine = InnoDB;
+
+ALTER TABLE member
+    ADD INDEX idx_generation_name (member_generation, member_name);
+
+ALTER TABLE attend
+    ADD INDEX idx_program (attend_program_id);
