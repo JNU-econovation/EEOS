0a389b98575f7a23eaf68fc743ec9347f9750a79
HyeonjiJang <jhg050908@gmail.com>
2023-11-05 20:42:00 +0900

    Revert "feat/AOS: typography 변수명 변경"
    This reverts commit 142e45cfc927b33912b4fd2f3429da973b2eb80e.

--- AOS/app/src/main/java/com/example/eeos/MainActivity.kt
+++ AOS/app/src/main/java/com/example/eeos/MainActivity.kt
@@ -3,14 +3,12 @@ package com.example.eeos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.eeos.ui.theme.EeosTheme
 
@@ -37,7 +35,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-    Image(painter = painterResource(id = R.drawable.common_mypage_button), contentDescription = "")
 }
 
 @Preview(showBackground = true)
