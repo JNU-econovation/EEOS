e03e3a8e25f201e0db443c738907744bb9b1a09e
HyeonjiJang <jhg050908@gmail.com>
2023-11-06 10:23:22 +0900

    refactor/AOS: 조건문 변경

--- AOS/app/src/main/java/com/example/eeos/ui/theme/Theme.kt
+++ AOS/app/src/main/java/com/example/eeos/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package com.example.eeos.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -45,7 +44,7 @@ fun EeosTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
