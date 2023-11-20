232564cd29c3e881f9777600a082e5e60ce2c0e9
HyeonjiJang <jhg050908@gmail.com>
2023-11-20 12:25:01 +0900

    delete/AOS: 불필요한 데이터 삭제

--- AOS/app/src/main/java/com/example/eeos/ui/home/CategoryChipData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.eeos.ui.home
-
-data class CategoryChipData(
-    val categoryChipName: String,
-    val isChipSelected: Boolean
-)
--- AOS/app/src/main/java/com/example/eeos/ui/home/ProgramStatusChipData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.eeos.ui.home
-
-data class ProgramStatusChipData(
-    val isEnd: Boolean,
-    val isSelected: Boolean
-)
