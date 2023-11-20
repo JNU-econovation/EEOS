394fcaad208c9f077ec9774e3edae1f2237f79bc
bada308 <203190@jnu.ac.kr>
2023-11-20 20:38:11 +0900

    feat: SubLayout 제거 - Header 디자인 통일

--- FE/app/detail/[programId]/page.tsx
+++ FE/app/detail/[programId]/page.tsx
@@ -1,6 +1,6 @@
 import MemberList from "@/components/detail/MemberList.component";
 import ProgramInfo from "@/components/detail/ProgramInfo.component";
-import SubLayout from "@/components/layout/SubLayout";
+import DefaultLayout from "@/components/layout/DefaultLayout";
 
 interface ProgramDetailPageProps {
   params: {
@@ -12,13 +12,13 @@ const ProgramDetailPage = ({ params }: ProgramDetailPageProps) => {
   const { programId } = params;
 
   return (
-    <SubLayout right="btn">
+    <DefaultLayout>
       <ProgramInfo programId={programId} />
       <div className="mx-8 flex w-[98vw] flex-col items-center gap-10 bg-soft_secondary py-8">
         <MemberList key="attend" programId={programId} attendStatus="attend" />
         <MemberList key="absent" programId={programId} attendStatus="absent" />
       </div>
-    </SubLayout>
+    </DefaultLayout>
   );
 };
 
