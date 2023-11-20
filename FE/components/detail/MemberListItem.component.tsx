d83bba91dd9ad457a0f2e25c1739c19dd0271b2d
bada308 <203190@jnu.ac.kr>
2023-11-21 01:10:58 +0900

    feat: MemberListItem 디자인 수정

--- FE/components/detail/MemberListItem.component.tsx
+++ FE/components/detail/MemberListItem.component.tsx
@@ -1,32 +1,15 @@
-import classNames from "classnames";
-import Image from "next/image";
-import { attendStatus, defaultMember } from "@/src/apis/types/member";
+import { defaultMember } from "@/src/apis/types/member";
 
 interface MemberListItemProps {
   memberData: defaultMember;
-  attendStatus: attendStatus;
 }
 
 // TODO: MemberListItem 바뀐 디자인 적용
-const MemberListItem = ({ memberData, attendStatus }: MemberListItemProps) => {
-  const { generation, name } = memberData;
-
-  const memberListItemClass = classNames(
-    "flex h-[28px] w-[28px] items-center justify-center rounded-full",
-    {
-      "bg-tertiary-100": attendStatus === "attend",
-      "bg-error": attendStatus !== "attend",
-    },
-  );
+const MemberListItem = ({ memberData }: MemberListItemProps) => {
+  const { name } = memberData;
   return (
-    <div className="m-2 grid grid-cols-[3rem_7rem] border-b-[1px] border-gray-20 px-6 py-5">
-      <div className={memberListItemClass}>
-        <Image src="/icons/check.svg" alt="check" width={20} height={20} />
-      </div>
-      <div className="flex gap-2 self-center">
-        <span>{generation}기</span>
-        <span>{name}</span>
-      </div>
+    <div className="grid w-full grid-cols-1 justify-items-center px-9 py-6 text-lg">
+      <span>{name}</span>
     </div>
   );
 };
