56aec7a0e2eba09b4468c5cdf2a72df69d71493f
HyeonjiJang <jhg050908@gmail.com>
2023-11-21 10:49:40 +0900

    feat/AOS: SplashScreen 구현

---
+++ AOS/app/src/main/java/com/example/eeos/ui/splash/SplashScreen.kt
@@ -0,0 +1,36 @@
+package com.example.eeos.ui.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.eeos.R
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.splash_logo_default),
+            contentDescription = "Logo of EEOS",
+            modifier = Modifier.size(dimensionResource(id = R.dimen.size_common_screen_logo_120dp))
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun SplashScreenPreview() {
+    MaterialTheme {
+        SplashScreen()
+    }
+}
