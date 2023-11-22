e355eab84f9ea77c203f30db578f8ece0d04259d
bada308 <203190@jnu.ac.kr>
2023-11-22 17:39:18 +0900

    fix: change member list layout

--- FE/components/layout/DefaultLayout.tsx
+++ FE/components/layout/DefaultLayout.tsx
@@ -5,7 +5,7 @@ const DefaultLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="flex w-full flex-col items-center justify-center">
       <Header />
-      <main className="flex h-full w-full max-w-[300px] flex-col items-center sm:max-w-[600px] lg:max-w-[900px]">
+      <main className="flex h-full w-full max-w-[500px] flex-col items-center sm:max-w-[800px] lg:max-w-[1112px]">
         {children}
       </main>
     </div>
