87ebf0d23bfde5917be36f4c36ab222c253384d2
bada308 <203190@jnu.ac.kr>
2023-11-22 16:12:28 +0900

    feat: rename attendStatus - Toggle이 아닌 attendStatus Badge와 Selector 사용 - Toggle 컴포넌트 위치 이동

--- FE/components/common/attendStatus/AttendStatusSelector.component.tsx
+++ FE/components/common/attendStatus/AttendStatusSelector.component.tsx
@@ -1,16 +1,16 @@
-import CustomToggleItem from "./CustomToggleItem.component";
+import AttendStatusBadge from "./AttendStatusBadge.component";
 import { toggleOption } from "@/src/types/common/common";
 
-interface MultiToggleProps {
+interface AttendStatusSelectorProps {
   selectedValue: string;
   options: toggleOption[];
   onSelect: (value: string) => void;
 }
-const MultiToggle = ({
+const AttendStatusSelector = ({
   selectedValue,
   options,
   onSelect,
-}: MultiToggleProps) => {
+}: AttendStatusSelectorProps) => {
   const handleClick = (value: toggleOption) => {
     value.text !== selectedValue && onSelect(value.text);
   };
@@ -19,14 +19,14 @@ const MultiToggle = ({
     <div className="flex h-fit w-fit transform rounded-3xl bg-gray-10 delay-300">
       {options.map((option) => (
         <div onClick={() => handleClick(option)}>
-          <CustomToggleItem
+          <AttendStatusBadge
             key={option.text}
             text={option.text}
-            color={selectedValue === option.text ? option.color : "default"}
+            color={selectedValue === option.text ? option.color : "gray"}
           />
         </div>
       ))}
     </div>
   );
 };
-export default MultiToggle;
+export default AttendStatusSelector;
