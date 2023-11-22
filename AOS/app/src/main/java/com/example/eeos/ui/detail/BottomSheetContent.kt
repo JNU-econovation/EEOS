99c192903bbd5c4397c0c590d1f07579427a45b9
HyeonjiJang <jhg050908@gmail.com>
2023-11-23 01:07:00 +0900

    update/AOS: BottomSheet bar 삭제 및 위치 조정

--- AOS/app/src/main/java/com/example/eeos/ui/detail/BottomSheetContent.kt
+++ AOS/app/src/main/java/com/example/eeos/ui/detail/BottomSheetContent.kt
@@ -1,8 +1,8 @@
 package com.example.eeos.ui.detail
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -11,7 +11,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.eeos.R
@@ -19,27 +18,9 @@ import com.example.eeos.R
 @Composable
 fun BottomSheetContents() {
     Column(
+        modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Spacer(
-            modifier = Modifier.height(
-                height = dimensionResource(id = R.dimen.margin_detail_screen_space_bar_vertical)
-            )
-        )
-        Image(
-            painter = painterResource(id = R.drawable.detail_bottom_sheet_bar),
-            contentDescription = stringResource(R.string.detail_bottom_sheet_bar_description)
-        )
-        Spacer(
-            modifier = Modifier.height(
-                height = dimensionResource(id = R.dimen.margin_detail_screen_space_bar_vertical)
-            )
-        )
-        Spacer(
-            modifier = Modifier.height(
-                height = dimensionResource(id = R.dimen.margin_detail_screen_space_attend_status_vertical)
-            )
-        )
         AttendStatusInfo(
             memberInfo = MemberData(
                 generation = 24,
@@ -49,7 +30,7 @@ fun BottomSheetContents() {
         ) { RequestAttendCheckChip() }
         Spacer(
             modifier = Modifier.height(
-                height = dimensionResource(id = R.dimen.margin_detail_screen_space_attend_status_vertical)
+                height = dimensionResource(id = R.dimen.margin_detail_screen_space_between_info_and_chip)
             )
         )
         Spacer(
@@ -68,6 +49,11 @@ fun BottomSheetContents() {
             )
         )
         AttendStatusButtons()
+        Spacer(
+            modifier = Modifier.height(
+                height = dimensionResource(id = R.dimen.margin_detail_screen_space_attend_status_buttons_vertical)
+            )
+        )
     }
 }
 
