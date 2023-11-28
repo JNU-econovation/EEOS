0d15cbd6170e81382558b5193acbe2ff3087d461
kssumin <201566@jnu.ac.kr>
2023-11-28 19:59:41 +0900

    feat : 프로그램 시간 제외 날짜만 포함한 timestamp 반환하도록 변경

--- BE/eeos/src/main/java/com/blackcompany/eeos/common/utils/DateConverter.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/common/utils/DateConverter.java
@@ -28,6 +28,23 @@ public class DateConverter {
 		return Instant.ofEpochSecond(epochSecond).atZone(ZoneId.of("Asia/Seoul")).toLocalDate();
 	}
 
+	public static Timestamp toEpochSecond(Timestamp epochSecond) {
+		if (epochSecond == null) {
+			return null;
+		}
+
+		LocalDate localDate = toLocalDate(epochSecond);
+		return toEpochSecond(localDate);
+	}
+
+	public static LocalDate toLocalDate(Timestamp epochSecond) {
+		if (epochSecond == null) {
+			return null;
+		}
+
+		return epochSecond.toLocalDateTime().toLocalDate();
+	}
+
 	public static LocalDateTime toLocalDateTime(Long epochSecond) {
 		if (epochSecond == null) {
 			return null;
