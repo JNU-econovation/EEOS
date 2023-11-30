651d55a14114afd2cb9fbe6d523ac55b03e645d8
HyeonjiJang <jhg050908@gmail.com>
2023-11-30 15:06:34 +0900

    feat/AOS: Dto에 사용되는 Const 모음

---
+++ AOS/app/src/main/java/com/example/eeos/consts/Consts.kt
@@ -0,0 +1,32 @@
+package com.example.eeos.consts
+
+object ActiveStatus {
+    const val am = "am"
+    const val rm = "rm"
+    const val cm = "cm"
+    const val ob = "ob"
+}
+
+object Category {
+    const val weekly = "weekly"
+    const val president = "president"
+    const val event = "event"
+    const val etc = "etc"
+}
+
+object ProgramStatus {
+    const val active = "active"
+    const val end = "end"
+}
+
+object ProgramType {
+    const val demand = " demand"
+    const val notification = "notification"
+}
+
+object AttendStatus {
+    const val attend = "attend"
+    const val absent = "absent"
+    const val perceive = "perceive"
+    const val nonResponse = "nonResponse"
+}
