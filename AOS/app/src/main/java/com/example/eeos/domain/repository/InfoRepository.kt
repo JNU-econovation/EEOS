c63e0f1735609b2948af42e654bc623ce99ebc62
HyeonjiJang <jhg050908@gmail.com>
2023-12-01 16:19:45 +0900

    update/AOS: repository 반환 타입 수정

--- AOS/app/src/main/java/com/example/eeos/domain/repository/InfoRepository.kt
+++ AOS/app/src/main/java/com/example/eeos/domain/repository/InfoRepository.kt
@@ -1,11 +1,12 @@
 package com.example.eeos.domain.repository
 
+import com.example.eeos.data.model.remote.request.RequestPutActiveStatusDto
 import com.example.eeos.domain.model.ActiveStatus
 
 interface InfoRepository {
     suspend fun getActiveStatus(): Result<ActiveStatus>
 
     suspend fun putActiveStatus(
-        activeStatus: ActiveStatus
+        requestPutActiveStatusDto: RequestPutActiveStatusDto
     ): Result<Unit>
 }
--- AOS/app/src/main/java/com/example/eeos/domain/repository/ProgramRepository.kt
+++ AOS/app/src/main/java/com/example/eeos/domain/repository/ProgramRepository.kt
@@ -3,9 +3,10 @@ package com.example.eeos.domain.repository
 import com.example.eeos.consts.AttendStatus
 import com.example.eeos.consts.Category
 import com.example.eeos.consts.ProgramStatus
-import com.example.eeos.domain.model.MemberList
+import com.example.eeos.data.model.remote.request.RequestPutAttendStatusDto
+import com.example.eeos.domain.model.Member
+import com.example.eeos.domain.model.Program
 import com.example.eeos.domain.model.ProgramDetail
-import com.example.eeos.domain.model.ProgramList
 
 interface ProgramRepository {
     suspend fun getProgramDetail(
@@ -17,16 +18,15 @@ interface ProgramRepository {
         programStatus: ProgramStatus,
         size: Int,
         page: Int
-    ): Result<ProgramList>
+    ): Result<List<Program>>
 
     suspend fun putAttendStatus(
         programId: Int,
-        beforeAttendStatus: AttendStatus,
-        afterAttendStatus: AttendStatus
+        requestPutAttendStatusDto: RequestPutAttendStatusDto
     ): Result<Unit>
 
     suspend fun getMemberList(
         programId: Int,
         attendStatus: AttendStatus
-    ): Result<MemberList>
+    ): Result<List<Member>>
 }
