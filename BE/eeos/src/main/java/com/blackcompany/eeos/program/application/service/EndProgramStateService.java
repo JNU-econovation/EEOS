06acb9fe3ec666f92cb4ff0f57cb01c4176042d3
kssumin <201566@jnu.ac.kr>
2023-12-04 04:06:10 +0900

    feat : 각 programStatus가 어떤 ProgramStatus에 속하는지 알도록 구현

--- BE/eeos/src/main/java/com/blackcompany/eeos/program/application/service/EndProgramStateService.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/program/application/service/EndProgramStateService.java
@@ -1,15 +1,23 @@
 package com.blackcompany.eeos.program.application.service;
 
+import com.blackcompany.eeos.program.application.domain.ProgramStatus;
 import com.blackcompany.eeos.program.persistence.ProgramEntity;
 import com.blackcompany.eeos.program.persistence.ProgramRepository;
 import java.sql.Timestamp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
+@Service
 @RequiredArgsConstructor
 public class EndProgramStateService implements ProgramStateService {
 	private final ProgramRepository programRepository;
+
+	@Override
+	public ProgramStatus support() {
+		return ProgramStatus.END;
+	}
 
 	@Override
 	public Page<ProgramEntity> getPages(Timestamp now, PageRequest pageRequest) {
--- BE/eeos/src/main/java/com/blackcompany/eeos/program/application/service/ProgramStateService.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/program/application/service/ProgramStateService.java
@@ -1,10 +1,13 @@
 package com.blackcompany.eeos.program.application.service;
 
+import com.blackcompany.eeos.program.application.domain.ProgramStatus;
 import com.blackcompany.eeos.program.persistence.ProgramEntity;
 import java.sql.Timestamp;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
 public interface ProgramStateService {
+	ProgramStatus support();
+
 	Page<ProgramEntity> getPages(Timestamp now, PageRequest pageRequest);
 }
