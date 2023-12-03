43b4c05f692221c6e263202c701c2f6abe4808f9
HyeonjiJang <jhg050908@gmail.com>
2023-12-03 17:17:31 +0900

    style/AOS: ktlintFormat

--- AOS/app/src/main/java/com/example/eeos/di/RetrofitModule.kt
+++ AOS/app/src/main/java/com/example/eeos/di/RetrofitModule.kt
@@ -4,12 +4,12 @@ import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import javax.inject.Singleton
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Singleton
 
 @Module
 @InstallIn(Singleton::class)
@@ -17,7 +17,6 @@ object RetrofitModule {
     private const val MOCK_URL = "https://562ee14c-9738-4e79-ba16-f8d78480a890.mock.pstmn.io/api"
     private var gson = GsonBuilder().setLenient().create()
 
-    
     @Provides
     @Singleton
     fun provideHttpLoggingInterceptor(): Interceptor = HttpLoggingInterceptor().apply {
