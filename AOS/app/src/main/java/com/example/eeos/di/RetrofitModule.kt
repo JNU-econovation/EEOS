133bcf837c4a689fbb4d8ec6f75192c642079371
HyeonjiJang <jhg050908@gmail.com>
2023-12-03 20:35:38 +0900

    fix/AOS: RetrofitModule 오류 해결

--- AOS/app/src/main/java/com/example/eeos/di/RetrofitModule.kt
+++ AOS/app/src/main/java/com/example/eeos/di/RetrofitModule.kt
@@ -4,6 +4,7 @@ import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
@@ -12,7 +13,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
-@InstallIn(Singleton::class)
+@InstallIn(SingletonComponent::class)
 object RetrofitModule {
     private const val MOCK_URL = "https://562ee14c-9738-4e79-ba16-f8d78480a890.mock.pstmn.io/api"
     private var gson = GsonBuilder().setLenient().create()
