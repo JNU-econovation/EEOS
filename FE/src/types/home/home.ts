c8739aee4185fd6a0af6a378bc66de52d1468b2a
bada308 <203190@jnu.ac.kr>
2023-12-13 17:00:01 +0900

    feat: delete legacy code

--- FE/src/types/home/home.ts
+++
@@ -1,6 +0,0 @@
-export type programStatusEn = "ACTIVE" | "END";
-export type programStatusKr = "진행 중" | "종료";
-
-export type programStatusOptionType = {
-  [key in programStatusKr]: programStatusEn;
-};
