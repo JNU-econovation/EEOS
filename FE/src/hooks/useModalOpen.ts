7983a2808001ee8520ab1818f81c19aec72fd414
bada308 <203190@jnu.ac.kr>
2023-12-13 17:07:34 +0900

    feat(useModalOpen): 모달 개폐 관리 hook 구현

---
+++ FE/src/hooks/useModalOpen.ts
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const useModalOpen = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  return { isOpen, openModal, closeModal };
+};
+
+export default useModalOpen;
