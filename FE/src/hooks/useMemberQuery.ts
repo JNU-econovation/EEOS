cd8438474d6e08455d3ae7398dbc0e550f3b9f90
bada308 <203190@jnu.ac.kr>
2023-12-14 11:35:27 +0900

    feat: member, program, user query를 위한 hook 구현

---
+++ FE/src/hooks/useMemberQuery.ts
@@ -0,0 +1,44 @@
+import { useQuery } from "@tanstack/react-query";
+import {
+  getMembersByActiveStatus,
+  getProgramMembersByActiveStatus,
+  getProgramMembersByAttendStatus,
+} from "../apis/member";
+import API from "../constants/API";
+import { ActiveStatusWithAll, AttendStatus } from "../types/member";
+
+export const useGetMemberByActive = (activeStatus: ActiveStatusWithAll) => {
+  return useQuery({
+    queryKey: [API.MEMBER.LIST, activeStatus],
+    queryFn: () => getMembersByActiveStatus(activeStatus),
+  });
+};
+
+interface GetProgramMemebersByActive {
+  status: ActiveStatusWithAll;
+  programId: number;
+}
+interface GetProgramMemebersByAttend {
+  status: AttendStatus;
+  programId: number;
+}
+
+export const useGetProgramMembersByActive = ({
+  programId,
+  status,
+}: GetProgramMemebersByActive) => {
+  return useQuery({
+    queryKey: [API.MEMBER.PROGRAM(programId), status],
+    queryFn: () => getProgramMembersByActiveStatus(programId, status),
+  });
+};
+
+export const useGetProgramMembersByAttend = ({
+  programId,
+  status,
+}: GetProgramMemebersByAttend) => {
+  return useQuery({
+    queryKey: [API.MEMBER.PROGRAM, status],
+    queryFn: () => getProgramMembersByAttendStatus(programId, status),
+  });
+};
