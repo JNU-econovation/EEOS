79288437ab3a3cb8430de927519fcd2ec0005e6d
bada308 <203190@jnu.ac.kr>
2023-12-14 15:02:07 +0900

    feat: member, program type 추가

---
+++ FE/src/types/member.ts
@@ -0,0 +1,26 @@
+export type ActiveStatus = "am" | "rm" | "cm" | "ob";
+export type ActiveStatusWithAll = ActiveStatus | "all";
+export type AttendStatus =
+  | "attend"
+  | "absent"
+  | "perceive"
+  | "nonResponse"
+  | "nonRelated";
+
+export interface MemberInfo {
+  memberId: number;
+  name: string;
+  attendStatus: AttendStatus;
+  activeStatus: ActiveStatus;
+}
+
+export interface MemberAttendStatusInfo
+  extends Omit<MemberInfo, "activeStatus"> {}
+
+export interface MemberActiveStatusInfo
+  extends Omit<MemberInfo, "attendStatus"> {}
+
+export interface UserActiveStatusInfo
+  extends Omit<MemberInfo, "memberId" | "attendStatus"> {}
+export interface UserAttendStatusInfo
+  extends Omit<MemberInfo, "memberId" | "activeStatus"> {}
