370b0c51fdd63c5de69960fa74b9fbc27c8776e8
bada308 <203190@jnu.ac.kr>
2023-12-14 11:32:54 +0900

    feat: member, program, user API 추가

---
+++ FE/src/apis/user.ts
@@ -0,0 +1,73 @@
+import API from "../constants/API";
+import { ActiveStatus, AttendStatus } from "../types/member";
+import {
+  UserActiveStatusInfoDto,
+  UserAttendStatusInfoDto,
+} from "./dtos/user.dto";
+import { https } from "./instance";
+
+/**
+ * 본인의 출석 상태 조회
+ */
+
+export const getMyAttendStatus = async (
+  programId: number,
+): Promise<UserAttendStatusInfoDto> => {
+  const { data } = await https({
+    url: API.USER.ATTEND_STATUS(programId),
+    method: "GET",
+  });
+  return new UserAttendStatusInfoDto(data.data);
+};
+
+/**
+ * 본인의 출석 상태 변경
+ */
+
+export interface PutMyAttendStatusRequest {
+  beforeAttendStatus: AttendStatus;
+  afterAttendStatus: AttendStatus;
+}
+
+export const putMyAttendStatus = async (
+  programId: number,
+  body: PutMyAttendStatusRequest,
+) => {
+  const { data } = await https({
+    url: API.USER.ATTEND_STATUS(programId),
+    method: "PUT",
+    data: body,
+  });
+  return data.data;
+};
+
+/**
+ * 본인의 회원 상태 조회
+ */
+
+export const getMyActiveStatus = async (): Promise<UserActiveStatusInfoDto> => {
+  const { data } = await https({
+    url: API.USER.ACTIVE_STATUS,
+    method: "GET",
+  });
+  return new UserActiveStatusInfoDto(data.data);
+};
+
+/**
+ * 본인의 회원 상태 변경
+ */
+
+interface PutMyActiveStatusRequest {
+  activeStatus: ActiveStatus;
+}
+
+export const putMyActiveStatus = async (
+  body: PutMyActiveStatusRequest,
+): Promise<UserActiveStatusInfoDto> => {
+  const { data } = await https({
+    url: API.USER.ACTIVE_STATUS,
+    method: "PUT",
+    data: body,
+  });
+  return new UserActiveStatusInfoDto(data.data);
+};
