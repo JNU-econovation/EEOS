01c3fb8bbb6caab67fe553261bc226135d3513db
bada308 <203190@jnu.ac.kr>
2023-12-14 17:20:28 +0900

    feat: Not found 페이지 추가

---
+++ FE/src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import { ERROR } from "@/constants/ERROR";
+import ROUTES from "@/constants/ROUTES";
+import Image from "next/image";
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-16">
+      <h1 className="text-5xl font-bold text-black">{ERROR.TITLE}</h1>
+      <div className="mb-10 flex flex-col items-center gap-2">
+        {ERROR.DESCRIPTION.map((description) => (
+          <span className="text-lg font-normal">{description}</span>
+        ))}
+      </div>
+      <Image src={ERROR.IMAGE_URL} alt="error" width={270} height={210.7} />
+      <Link
+        href={ROUTES.HOME}
+        className="border-b-[3px] border-stroke-30 text-lg font-extrabold"
+      >
+        {ERROR.BUTTON_TEXT}
+      </Link>
+    </div>
+  );
+};
+export default NotFound;
