42982f0cab4f54c3e0253a7b4df03cfe188802f1
bada308 <203190@jnu.ac.kr>
2023-12-14 17:20:54 +0900

    feat: redirect 추가

--- FE/next.config.js
+++ FE/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // rewrite
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/main",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
