c133caee99967e7bf7f170549fcf8676eefd1805
bada308 <203190@jnu.ac.kr>
2023-12-14 18:52:31 +0900

    feat: 전역 레이아웃에 Header 추가

--- FE/src/app/layout.tsx
+++ FE/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "@/utils/provider";
+import Header from "@/components/common/header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Provider>{children}</Provider>
+        <Provider>
+          <Header />
+          {children}
+        </Provider>
       </body>
     </html>
   );
