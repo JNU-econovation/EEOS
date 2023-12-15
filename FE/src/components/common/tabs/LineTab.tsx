891ada80ffee23e6d7d3daf2f51655447e3c27b9
bada308 <203190@jnu.ac.kr>
2023-12-15 13:38:01 +0900

    feat: LineTab component 추가

---
+++ FE/src/components/common/tabs/LineTab.tsx
@@ -0,0 +1,43 @@
+import TabItem, { tabColors, tabSizes } from "./TabItem";
+
+interface Option<T> {
+  type: T;
+  text: string;
+}
+
+interface Props<T> {
+  options: Option<T>[];
+  selected: T;
+  setSelected: (selected: T) => void;
+  size: keyof typeof tabSizes;
+  baseColor: keyof typeof tabColors;
+  potionColor: keyof typeof tabColors;
+}
+
+const LineTab = <T,>({
+  options,
+  selected,
+  setSelected,
+  size,
+  baseColor,
+  potionColor,
+}: Props<T>) => {
+  const getColor = (option: Option<T>) => {
+    return option.type === selected ? potionColor : baseColor;
+  };
+
+  return (
+    <div className="flex gap-4">
+      {options.map((option: Option<T>) => (
+        <TabItem
+          color={getColor(option)}
+          size={size}
+          rounded
+          text={option.text}
+          onClick={() => setSelected(option.type)}
+        />
+      ))}
+    </div>
+  );
+};
+export default LineTab;
