417a229268dadedec5db55ccddec38bc4001130f
bada308 <203190@jnu.ac.kr>
2023-12-15 13:38:39 +0900

    feat: tab colors & sizes export

--- FE/src/components/common/tabs/TabItem.tsx
+++ FE/src/components/common/tabs/TabItem.tsx
@@ -1,6 +1,6 @@
 import classNames from "classnames";
 
-const colors = {
+export const tabColors = {
   gray: "bg-gray-10 text-gray-30 border-gray-20",
   yellow: "bg-warning-10 text-warning-30 border-warning-30",
   teal: "bg-secondary-20 text-tertiary-20 border-tertiary-20",
@@ -8,24 +8,24 @@ const colors = {
   navy: "bg-paragraph text-background border-paragraph",
 };
 
-const sizes = {
+export const tabSizes = {
   sm: "min-w-[4.25rem] px-2 py-[0.3rem] text-xs",
   md: "min-w-[5rem] px-3 py-2 text-sm",
   lg: "min-w-[6rem] px-4 py-2 text-base",
 };
 
 interface Props {
-  color: keyof typeof colors;
+  color: keyof typeof tabColors;
   rounded: boolean;
-  size: keyof typeof sizes;
+  size: keyof typeof tabSizes;
   text: string;
   onClick?: () => void;
 }
 const TabItem = ({ color, rounded, size, text, onClick }: Props) => {
   const tabItemStyle = classNames(
     "flex h-fit w-fit cursor-pointer items-center justify-center border-2 font-semibold",
-    colors[color],
-    sizes[size],
+    tabColors[color],
+    tabSizes[size],
     {
       "rounded-full": rounded,
       "rounded-md": !rounded,
