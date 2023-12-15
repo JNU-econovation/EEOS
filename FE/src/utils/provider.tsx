15d621873313f79204e95b36d1e721f6d1daafed
bada308 <203190@jnu.ac.kr>
2023-12-15 15:45:28 +0900

    feat: queries에 suspense true 적용

--- FE/src/utils/provider.tsx
+++ FE/src/utils/provider.tsx
@@ -3,7 +3,14 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
-  const client = new QueryClient();
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        suspense: true,
+      },
+    },
+  });
+
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 };
 
