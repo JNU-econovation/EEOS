4be36ab36f3a8ce6b35ff932874891bb63114af2
bada308 <203190@jnu.ac.kr>
2023-12-17 14:00:51 +0900

    feat: 문자형 Tab 컴포넌트 추가

---
+++ FE/src/components/common/tabs/TextTab.tsx
@@ -0,0 +1,28 @@
+import { TabOption } from "@/types/tab";
+
+interface TextTabProps<T> {
+  options: TabOption<T>[];
+  selected: string;
+  onClick: (selected: T) => void;
+}
+
+const TextTab = <T,>({ options, selected, onClick }: TextTabProps<T>) => {
+  return (
+    <div className="flex w-full gap-4 border-b-2 border-stroke-30 px-2 py-4">
+      {options.map((option) =>
+        option.type === selected ? (
+          <span className="text-xl font-bold">{option.text}</span>
+        ) : (
+          <span
+            className="text-lg font-normal text-gray-600"
+            onClick={() => onClick(option.type)}
+          >
+            {option.text}
+          </span>
+        ),
+      )}
+    </div>
+  );
+};
+
+export default TextTab;
