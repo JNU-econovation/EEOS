9c562d91e31e55c8e4a61d5a89d234b354868bb3
bada308 <203190@jnu.ac.kr>
2023-12-19 15:27:41 +0900

    feat: ProgramInfo 컴포넌트 추가 - 제목, 타입, 마감기한 등을 포함하는 ProgramHeader - 프로그램 내용을 포함하는 ProgramDetail

---
+++ FE/src/components/programDetail/program/ProgramHeader.tsx
@@ -0,0 +1,50 @@
+import { ProgramInfoDto } from "@/apis/dtos/program.dto";
+import Title from "@/components/common/Title";
+import TabItem from "@/components/common/tabs/TabItem";
+import PROGRAM from "@/constants/PROGRAM";
+import ROUTES from "@/constants/ROUTES";
+import { convertDate } from "@/utils/convert";
+import Image from "next/image";
+import Link from "@/components/common/Link";
+
+interface ProgramHeaderProps {
+  data: ProgramInfoDto;
+}
+
+const DEADLINE_TEXT = "마감기한 : ";
+
+const ProgramHeader = ({ data }: ProgramHeaderProps) => {
+  const { category, title, deadLine, programId } = data;
+
+  const categoryText = PROGRAM.CATEGORY_TAB[category].text;
+
+  return (
+    <section className="space-y-4 border-b-2 py-4">
+      <TabItem color="yellow" size="sm" text={categoryText} rounded />
+      <Title text={title} />
+      <div className="flex justify-between">
+        <p className="text-lg">{DEADLINE_TEXT + convertDate(deadLine)}</p>
+        <div className="flex items-end gap-6">
+          <Link href={ROUTES.EDIT(programId)}>
+            <Image
+              src="/icons/pencil.svg"
+              alt="프로그램 수정"
+              width={22}
+              height={22}
+              className="h-[22px] w-[22px] hover:cursor-pointer"
+            />
+          </Link>
+          <Image
+            src="/icons/trash.svg"
+            alt="프로그램 삭제"
+            width={22}
+            height={22}
+            style={{ width: 22, height: 22 }}
+            className="h-[22px] w-[22px] hover:cursor-pointer"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default ProgramHeader;
