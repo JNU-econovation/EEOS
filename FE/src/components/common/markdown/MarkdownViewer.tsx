daede3b69ec7ee424c0bd21f7713b209bf97adca
bada308 <203190@jnu.ac.kr>
2023-12-19 15:26:36 +0900

    feat: MarkdownViewer 컴포넌트 추가

---
+++ FE/src/components/common/markdown/MarkdownViewer.tsx
@@ -0,0 +1,50 @@
+import classNames from "classnames";
+import Markdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import remarkGfm from "remark-gfm";
+import "./markdown-editor.styles.css";
+
+const MarkdownViewer = ({
+  value,
+  className = "",
+}: {
+  value: string;
+  className?: string;
+}) => {
+  const markdownClass = classNames(
+    "markdown-body h-full overflow-y-auto p-4",
+    className,
+  );
+  return (
+    <Markdown
+      className={markdownClass}
+      remarkPlugins={[remarkGfm]}
+      components={{
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        code(props: any) {
+          const { className, children, inline } = props;
+          const match = /language-(\w+)/.exec(className || "");
+
+          return !inline && match ? (
+            <SyntaxHighlighter
+              style={materialLight}
+              PreTag="div"
+              language={match[1]}
+              children={String(children).replace(/\n$/, "")}
+              {...props}
+            />
+          ) : (
+            <code className={className || ""} {...props}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
+      {value}
+    </Markdown>
+  );
+};
+
+export default MarkdownViewer;
