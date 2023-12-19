57e1c0df5c1d86705e6863f73db8e027155ab9d7
bada308 <203190@jnu.ac.kr>
2023-12-19 16:38:29 +0900

    feat: ProgramDemandCheckBox 컴포넌트 추가

---
+++ FE/src/components/common/form/ProgramDemandCheckBox.tsx
@@ -0,0 +1,29 @@
+interface ProgramDemandCheckBoxProps {
+  disabled: boolean;
+  isDemand: boolean;
+  onClick: (v: boolean) => void;
+}
+
+const ProgramDemandCheckBox = ({
+  disabled,
+  isDemand,
+  onClick,
+}: ProgramDemandCheckBoxProps) => {
+  return (
+    <>
+      {!disabled && (
+        <div className="absolute right-0 top-0 flex gap-2">
+          <label className="text-sm font-bold">수요조사 등록하기</label>
+          {/* TODO: Checkbox 컴포넌트로 변경 */}
+          <input
+            type="checkbox"
+            className="accent-primary"
+            checked={isDemand}
+            onChange={() => onClick(!isDemand)}
+          />
+        </div>
+      )}
+    </>
+  );
+};
+export default ProgramDemandCheckBox;
