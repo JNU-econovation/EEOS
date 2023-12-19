4a67370307bb4e92dec2eaef4c87e2fa40b59612
bada308 <203190@jnu.ac.kr>
2023-12-19 15:33:39 +0900

    rename: from UserInfoModal to UserActiveModal

--- FE/src/components/common/header/UserActiveModal.loader.tsx
+++ FE/src/components/common/header/UserActiveModal.loader.tsx
@@ -1,6 +1,6 @@
 import LoadingSpinner from "../LoadingSpinner";
 
-const UserInfoModalSkeleton = () => {
+const UserActiveModalSkeleton = () => {
   return (
     <section className="absolute -left-32 top-10 flex h-80 w-80 flex-col items-center gap-6 rounded-2xl bg-background px-12 py-6 drop-shadow-lg">
       <LoadingSpinner />
@@ -8,4 +8,4 @@ const UserInfoModalSkeleton = () => {
   );
 };
 
-export default UserInfoModalSkeleton;
+export default UserActiveModalSkeleton;
