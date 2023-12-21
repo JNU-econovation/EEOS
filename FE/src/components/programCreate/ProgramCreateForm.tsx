c08014a6a088d0e4f0c76b861574c1c1844e5b8c
bada308 <203190@jnu.ac.kr>
2023-12-21 16:26:10 +0900

    feat: ProgramCreateForm 컴포넌트 구현

--- FE/src/components/programCreate/ProgramCreateForm.tsx
+++ FE/src/components/programCreate/ProgramCreateForm.tsx
@@ -1,6 +1,20 @@
+"use client";
+
 import ProgramForm from "../common/form/ProgramForm";
+import MemberTable from "../common/memberTable/MemberTable";
+import useProgramFormData from "@/hooks/useProgramFormData";
 
 const ProgramCreateForm = () => {
-  return <ProgramForm formType="create" />;
+  const formData = useProgramFormData();
+
+  return (
+    <ProgramForm formType="create" formData={formData}>
+      <MemberTable
+        formType="create"
+        members={formData.members}
+        setMembers={formData.setMembers}
+      />
+    </ProgramForm>
+  );
 };
 export default ProgramCreateForm;
