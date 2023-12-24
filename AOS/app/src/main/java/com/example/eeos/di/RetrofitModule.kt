10a51c29ed9cc584e91d316840e62fa4248989f3
HyeonjiJang <jhg050908@gmail.com>
2023-12-24 16:44:54 +0900

    update/AOS: RetrofitModule에 HttpLoggingInterceptor 추가

--- AOS/app/src/main/java/com/example/eeos/di/RetrofitModule.kt
+++ AOS/app/src/main/java/com/example/eeos/di/RetrofitModule.kt
@@ -12,10 +12,11 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 @Module
 @InstallIn(SingletonComponent::class)
 object RetrofitModule {
-    private const val MOCK_URL = "https://562ee14c-9738-4e79-ba16-f8d78480a890.mock.pstmn.io/api"
+    private const val MOCK_URL = "https://562ee14c-9738-4e79-ba16-f8d78480a890.mock.pstmn.io/api/"
     private var gson = GsonBuilder().setLenient().create()
 
     @Provides
@@ -27,6 +28,9 @@ object RetrofitModule {
     @Provides
     @Singleton
     fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
         .build()
 
     @Provides
