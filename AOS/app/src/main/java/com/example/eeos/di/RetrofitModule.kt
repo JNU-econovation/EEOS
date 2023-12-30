75f38707f6ce28f7e02f79cf42ea737549549afc
HyeonjiJang <jhg050908@gmail.com>
2023-12-31 02:01:01 +0900

    update/AOS: RetrofitModule에 개발 서버 URL 추가

--- AOS/app/src/main/java/com/example/eeos/di/RetrofitModule.kt
+++ AOS/app/src/main/java/com/example/eeos/di/RetrofitModule.kt
@@ -17,6 +17,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 @InstallIn(SingletonComponent::class)
 object RetrofitModule {
     private const val MOCK_URL = "https://562ee14c-9738-4e79-ba16-f8d78480a890.mock.pstmn.io/api/"
+    private const val BE_DEV_URL = "https://be.dev.eeos.store/api/"
     private var gson = GsonBuilder().setLenient().create()
 
     @Provides
