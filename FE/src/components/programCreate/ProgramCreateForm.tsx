b8570775e8ca6677ae95951f3a9719985624f3d0
bada308 <203190@jnu.ac.kr>
2024-01-02 13:26:28 +0900

    feat(ProgramCreateForm): member 선택 함수 추가

--- FE/src/components/programCreate/ProgramCreateForm.tsx
+++ FE/src/components/programCreate/ProgramCreateForm.tsx
@@ -1,18 +1,28 @@
 "use client";
 
+import { useState } from "react";
 import ProgramForm from "../common/form/ProgramForm";
 import MemberTable from "../common/memberTable/MemberTable";
 import useProgramFormData from "@/hooks/useProgramFormData";
 
 const ProgramCreateForm = () => {
   const formData = useProgramFormData();
+  const [members, setMembers] = useState<Set<number>>(new Set<number>());
+
+  const updateMembers = (memberId: number) => {
+    const newMembers = new Set<number>(members);
+    newMembers.has(memberId)
+      ? newMembers.delete(memberId)
+      : newMembers.add(memberId);
+    setMembers(newMembers);
+  };
 
   return (
     <ProgramForm formType="create" formData={formData}>
       <MemberTable
         formType="create"
-        members={formData.members}
-        setMembers={formData.setMembers}
+        members={members}
+        setMembers={updateMembers}
       />
     </ProgramForm>
   );
