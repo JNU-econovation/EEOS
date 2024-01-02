26d084bf6f62ca2f074393d6d2c059c07deecf3e
bada308 <203190@jnu.ac.kr>
2024-01-02 19:20:04 +0900

    feat(ProgramCreateForm): Form Submit 구현

--- FE/src/components/programCreate/ProgramCreateForm.tsx
+++ FE/src/components/programCreate/ProgramCreateForm.tsx
@@ -5,11 +5,24 @@ import ProgramForm from "../common/form/ProgramForm";
 import MemberTable from "../common/memberTable/MemberTable";
 import useProgramFormData from "@/hooks/useProgramFormData";
 import { useQueryClient } from "@tanstack/react-query";
+import { useCreateProgram } from "@/hooks/query/useProgramQuery";
 
 const ProgramCreateForm = () => {
   const queryClient = useQueryClient();
   const formData = useProgramFormData();
   const [members, setMembers] = useState<Set<number>>(new Set<number>());
+
+  const { mutate: createProgramMutate } = useCreateProgram({
+    programData: {
+      members: Array.from(members, (memberId) => ({ memberId })),
+      title: formData.title,
+      deadLine: formData.deadLine,
+      content: formData.content,
+      category: formData.category,
+      type: formData.type,
+    },
+    formReset: formData.reset,
+  });
 
   const updateMembers = (memberId: number) => {
     const newMembers = new Set<number>(members);
@@ -31,8 +44,23 @@ const ProgramCreateForm = () => {
     setMembers(newMembers);
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (
+      !formData.title ||
+      !formData.content ||
+      !formData.deadLine ||
+      !formData.category ||
+      !formData.type
+    ) {
+      alert("모든 항목을 입력해주세요.");
+      return;
+    }
+    createProgramMutate();
+  };
+
   return (
-    <ProgramForm formType="create" formData={formData}>
+    <ProgramForm formType="create" onSubmit={handleSubmit} {...formData}>
       <MemberTable
         formType="create"
         members={members}
