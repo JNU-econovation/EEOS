10e87c14366c59cc3b6b79c22af853f22a5cb7bb
kssumin <201566@jnu.ac.kr>
2024-01-02 16:52:50 +0900

    feat : AuthInfoRepository 구현

---
+++ BE/eeos/src/main/java/com/blackcompany/eeos/auth/persistence/AuthInfoRepository.java
@@ -0,0 +1,8 @@
+package com.blackcompany.eeos.auth.persistence;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthInfoRepository extends JpaRepository<AuthInfoEntity, Long> {
+	Optional<AuthInfoEntity> findByMemberIdAndToken(Long memberId, String token);
+}
