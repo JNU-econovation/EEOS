800afce2600bb705f06b7cfe32c7d8b8ec971b6d
kssumin <201566@jnu.ac.kr>
2024-01-02 15:26:44 +0900

    feat : token 요청 시 응답 response 구현

---
+++ BE/eeos/src/main/java/com/blackcompany/eeos/auth/infra/oauth/slack/dto/SlackToken.java
@@ -0,0 +1,43 @@
+package com.blackcompany.eeos.auth.infra.oauth.slack.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SlackToken implements SlackApiResponse {
+	@JsonProperty("ok")
+	private boolean ok;
+
+	@JsonProperty("authed_user")
+	private AuthedUser authedUser;
+
+	private String error;
+
+	@Override
+	public String getError() {
+		return error;
+	}
+
+	@Getter
+	public static class AuthedUser {
+		@JsonProperty("id")
+		private String userId;
+
+		@JsonProperty("access_token")
+		private String accessToken;
+	}
+
+	public String getUserId() {
+		return getAuthedUser().getUserId();
+	}
+
+	public String getToken() {
+		return getAuthedUser().getAccessToken();
+	}
+}
