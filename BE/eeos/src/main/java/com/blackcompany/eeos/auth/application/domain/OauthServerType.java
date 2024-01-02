c4136c794690516499548d40a31ea238a7ce4012
kssumin <201566@jnu.ac.kr>
2024-01-02 15:24:09 +0900

    feat : oauth server type을 관리하는 enum 생성

--- BE/eeos/src/main/java/com/blackcompany/eeos/auth/application/domain/OauthServerType.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/auth/application/domain/OauthServerType.java
@@ -2,7 +2,9 @@ package com.blackcompany.eeos.auth.application.domain;
 
 import com.blackcompany.eeos.auth.application.exception.NotFoundOauthServerException;
 import java.util.Arrays;
+import lombok.Getter;
 
+@Getter
 public enum OauthServerType {
 	SLACK("slack");
 
@@ -12,10 +14,17 @@ public enum OauthServerType {
 		this.oauthServer = oauthServer;
 	}
 
-	public static OauthServerType find(String oauthServer) {
+	public static OauthServerType find(String type) {
 		return Arrays.stream(values())
-				.filter(oauthServerType -> oauthServerType.oauthServer.equals(oauthServer))
+				.filter(oauthServerType -> oauthServerType.oauthServer.equals(type))
 				.findAny()
-				.orElseThrow(() -> new NotFoundOauthServerException(oauthServer));
+				.orElseThrow(() -> new NotFoundOauthServerException(type));
+	}
+
+	public static OauthServerType find(OauthServerType type) {
+		return Arrays.stream(values())
+				.filter(oauthServerType -> oauthServerType.equals(type))
+				.findAny()
+				.orElseThrow(() -> new NotFoundOauthServerException(type.getOauthServer()));
 	}
 }
