e3e994339634d3ec8f1048c5093ef50df851010b
kssumin <201566@jnu.ac.kr>
2024-01-02 15:25:10 +0900

    feat : openFeign을 통한 slack api 호출 구현

---
+++ BE/eeos/src/main/java/com/blackcompany/eeos/auth/infra/oauth/slack/client/SlackApiClientImpl.java
@@ -0,0 +1,20 @@
+package com.blackcompany.eeos.auth.infra.oauth.slack.client;
+
+import com.blackcompany.eeos.auth.infra.oauth.slack.dto.SlackMember;
+import com.blackcompany.eeos.auth.infra.oauth.slack.dto.SlackToken;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "SlackOpenFeign", url = "https://slack.com/api")
+public interface SlackApiClientImpl extends SlackApiClient {
+	@GetMapping(path = "/oauth.v2.access")
+	SlackToken fetchToken(
+			@RequestParam("client_id") String clientId,
+			@RequestParam("code") String code,
+			@RequestParam("client_secret") String clientSecret);
+
+	@GetMapping(path = "/users.profile.get")
+	SlackMember fetchMember(@RequestHeader("Authorization") String token);
+}
