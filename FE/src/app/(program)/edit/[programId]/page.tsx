c2d243715b7e5c314a0c15156303fd0fdc00a93c
bada308 <203190@jnu.ac.kr>
2024-01-02 14:05:56 +0900

    feat: edit 페이지 구현

--- FE/src/app/(program)/edit/[programId]/page.tsx
+++ FE/src/app/(program)/edit/[programId]/page.tsx
@@ -1,7 +1,30 @@
-const ProgramEditPage = () => {
+"use client";
+
+import Title from "@/components/common/Title";
+import ProgramEditForm from "@/components/programEdit/ProgramEditForm";
+import { useGetProgramById } from "@/hooks/query/useProgramQuery";
+
+interface ProgramEditPageProps {
+  params: {
+    programId: string;
+  };
+}
+
+const ProgramEditPage = ({ params }: ProgramEditPageProps) => {
+  const { programId } = params;
+  const {
+    data: programInfo,
+    isLoading,
+    isError,
+  } = useGetProgramById(+programId);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error!</div>;
+
   return (
-    <div>
-      <h1>Edit Program</h1>
+    <div className="space-y-12">
+      <Title text="행사 수정" />
+      <ProgramEditForm programId={programId} programInfo={programInfo} />
     </div>
   );
 };
