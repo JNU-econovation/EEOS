7f96f553058f1707a76dc2aa4285d381b5bb1c12
kssumin <201566@jnu.ac.kr>
2024-01-04 02:27:24 +0900

    feat : 전달받은 status에 맞는 ActiveStatus 반환

--- BE/eeos/src/main/java/com/blackcompany/eeos/member/application/model/ActiveStatus.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/member/application/model/ActiveStatus.java
@@ -1,13 +1,28 @@
 package com.blackcompany.eeos.member.application.model;
 
+import com.blackcompany.eeos.auth.application.exception.NotFoundOauthServerException;
+import java.util.Arrays;
+import lombok.Getter;
+
+@Getter
 public enum ActiveStatus {
 	AM("am"),
 	CM("cm"),
 	RM("rm");
+	private final String status;
 
 	ActiveStatus(String status) {
 		this.status = status;
 	}
 
-	private final String status;
+	public static ActiveStatus find(String status) {
+		return Arrays.stream(values())
+				.filter(activeStatus -> activeStatus.status.equals(status))
+				.findAny()
+				.orElseThrow(() -> new NotFoundOauthServerException(status));
+	}
+
+	public String getStatus() {
+		return status;
+	}
 }
