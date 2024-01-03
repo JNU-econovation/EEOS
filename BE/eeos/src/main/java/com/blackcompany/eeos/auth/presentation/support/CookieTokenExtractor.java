86ac23e2a63334093f7d511379ad3129f0f83468
kssumin <201566@jnu.ac.kr>
2024-01-03 13:27:56 +0900

    feat : 쿠키에 토큰이 존재하는지 검증하고 반환하도록 구현

---
+++ BE/eeos/src/main/java/com/blackcompany/eeos/auth/presentation/support/CookieTokenExtractor.java
@@ -0,0 +1,42 @@
+package com.blackcompany.eeos.auth.presentation.support;
+
+import com.blackcompany.eeos.auth.application.exception.NotFoundCookieException;
+import java.util.Objects;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieTokenExtractor implements TokenExtractor {
+	private static final String COOKIE_KEY = "token";
+
+	@Override
+	public String extract(HttpServletRequest request) {
+		Cookie[] cookies = getCookies(request);
+
+		for (Cookie cookie : cookies) {
+			if (Objects.equals(COOKIE_KEY, cookie.getName())) {
+				return getValue(cookie.getValue());
+			}
+		}
+
+		throw new NotFoundCookieException();
+	}
+
+	private Cookie[] getCookies(HttpServletRequest request) {
+		Cookie[] cookies = request.getCookies();
+
+		if (cookies == null) {
+			throw new NotFoundCookieException();
+		}
+
+		return cookies;
+	}
+
+	private String getValue(String value) {
+		if (value != null) {
+			return value;
+		}
+		throw new NotFoundCookieException();
+	}
+}
