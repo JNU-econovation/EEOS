88021d0852bff2cb2013c205cc07a26e109920e0
kssumin <201566@jnu.ac.kr>
2024-01-05 03:32:36 +0900

    fix : activeStatus 타입으로 전달하도록 변경

--- BE/eeos/src/main/java/com/blackcompany/eeos/member/persistence/MemberRepository.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/member/persistence/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.blackcompany.eeos.member.persistence;
 
+import com.blackcompany.eeos.member.application.model.ActiveStatus;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,5 +13,5 @@ public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 	List<MemberEntity> findMembersByProgramId(@Param("programId") Long programId);
 
 	@Query("SELECT m FROM MemberEntity m where m.activeStatus= :activeStatus ORDER BY m.name")
-	List<MemberEntity> findMembersByActiveStatus(@Param("activeStatus") String activeStatus);
+	List<MemberEntity> findMembersByActiveStatus(@Param("activeStatus") ActiveStatus activeStatus);
 }
