214d570bab53d4d9a62a2d3f5624ba577399ee5b
kssumin <201566@jnu.ac.kr>
2024-01-05 02:26:15 +0900

    feat : activeStatus null validate 추가

--- BE/eeos/src/main/java/com/blackcompany/eeos/member/application/dto/ChangeActiveStatusRequest.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/member/application/dto/ChangeActiveStatusRequest.java
@@ -1,6 +1,9 @@
 package com.blackcompany.eeos.member.application.dto;
 
 import com.blackcompany.eeos.common.support.dto.AbstractResponseDto;
+import com.blackcompany.eeos.member.application.support.ActiveStatusSupplier;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +13,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder(toBuilder = true)
-public class ChangeAcitveStatusRequest implements AbstractResponseDto {
-	private String activeStatus;
+public class ChangeActiveStatusRequest implements AbstractResponseDto, ActiveStatusSupplier {
+	@NotNull @NotEmpty private String activeStatus;
 }
