f1e48fa0610c36d4cf4fd0b20c08fced998c3df6
kssumin <201566@jnu.ac.kr>
2024-01-05 02:32:05 +0900

    feat : activeStatus 테스트코드 작성

---
+++ BE/eeos/src/test/java/com/blackcompany/eeos/member/application/model/ActiveStatusTest.java
@@ -0,0 +1,34 @@
+package com.blackcompany.eeos.member.application.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.blackcompany.eeos.member.application.exception.NotFoundActiveStatusException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ActiveStatusTest {
+
+	@Test
+	@DisplayName("존재하지 않은 활동 상태일 때 예외가 발생한다.")
+	void not_found_active_status() {
+		// given
+		String status = "known";
+
+		// when & then
+		Assertions.assertThrows(NotFoundActiveStatusException.class, () -> ActiveStatus.find(status));
+	}
+
+	@Test
+	@DisplayName("존재하는 활동 상태일 때 해당 활동 상태를 반환한다.")
+	void found_active_status() {
+		// given
+		String status = "am";
+
+		// when
+		ActiveStatus activeStatus = ActiveStatus.find(status);
+
+		// then
+		assertEquals(ActiveStatus.AM, activeStatus);
+	}
+}
