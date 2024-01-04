1b76c3ae2c66fe0985e9f56a6416ebdb91adddc2
bada308 <203190@jnu.ac.kr>
2024-01-05 02:34:06 +0900

    feat: 페이지네이션 컴포넌트 위치 변경 - from page to ProgramList

--- FE/src/components/main/ProgramList.tsx
+++ FE/src/components/main/ProgramList.tsx
@@ -3,18 +3,23 @@ import { useGetProgramList } from "@/hooks/query/useProgramQuery";
 import { ProgramCategoryWithAll, ProgramStatus } from "@/types/program";
 import ProgramListItem from "./ProgramListItem";
 import ProgramListLoader from "./ProgramList.loader";
+import { useQueryClient } from "@tanstack/react-query";
+import Paginataion from "../common/pagination/Pagination";
 
 interface ProgramListProps {
   category?: ProgramCategoryWithAll;
   programStatus?: ProgramStatus;
   page?: number;
+  setPage: (page: number) => void;
 }
 
 const ProgramList = ({
   category = "all",
   programStatus = "active",
   page = 1,
+  setPage: handleSetPage,
 }: ProgramListProps) => {
+  const queryClient = useQueryClient();
   const {
     data: programListData,
     isLoading,
@@ -29,14 +34,23 @@ const ProgramList = ({
   if (isLoading) return <ProgramListLoader />;
   if (isError) return <div>Error...</div>;
 
+  queryClient.setQueryData<number>(["totalPage"], programListData.totalPage);
+
   const { programs } = programListData;
 
   return (
-    <div className="w-full space-y-5">
-      {programs.map((program) => (
-        <ProgramListItem key={program.programId} programData={program} />
-      ))}
-    </div>
+    <>
+      <div className="w-full space-y-5">
+        {programs.map((program) => (
+          <ProgramListItem key={program.programId} programData={program} />
+        ))}
+      </div>
+      <Paginataion
+        totalPage={programListData.totalPage}
+        currentPage={page}
+        onChange={(v) => handleSetPage(v)}
+      />
+    </>
   );
 };
 export default ProgramList;
