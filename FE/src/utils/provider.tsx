bb5ab1a10b9fd985f898fa00e8b3b2ff198a1b53
bada308 <203190@jnu.ac.kr>
2024-01-05 11:51:43 +0900

    feat: 전역 suspense 설정 제거

--- FE/src/utils/provider.tsx
+++ FE/src/utils/provider.tsx
@@ -4,13 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 
 const Provider = ({ children }: PropsWithChildren<{}>) => {
-  const client = new QueryClient({
-    defaultOptions: {
-      queries: {
-        suspense: true,
-      },
-    },
-  });
+  const client = new QueryClient();
 
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 };
