9f35d135f928ab11733b98d853104f30be3a6ed9
kssumin <201566@jnu.ac.kr>
2024-01-06 01:57:50 +0900

    refactor : api 명세에 맞춰 response 형식 변경

--- BE/eeos/src/main/java/com/blackcompany/eeos/program/application/model/converter/ProgramEntityConverter.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/program/application/model/converter/ProgramEntityConverter.java
@@ -1,8 +1,11 @@
 package com.blackcompany.eeos.program.application.model.converter;
 
 import com.blackcompany.eeos.common.support.converter.AbstractEntityConverter;
+import com.blackcompany.eeos.program.application.dto.ProgramsResponse;
 import com.blackcompany.eeos.program.application.model.ProgramModel;
 import com.blackcompany.eeos.program.persistence.ProgramEntity;
+import java.util.stream.Collectors;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,6 +19,15 @@ public class ProgramEntityConverter
 				.title(source.getTitle())
 				.programDate(source.getProgramDate())
 				.content(source.getContent())
+				.programCategory(source.getProgramCategory())
+				.programType(source.getProgramType())
+				.build();
+	}
+
+	public ProgramsResponse from(Page<ProgramEntity> pages) {
+		return ProgramsResponse.builder()
+				.page(pages)
+				.programs(pages.getContent().stream().map(this::from).collect(Collectors.toList()))
 				.build();
 	}
 
@@ -26,6 +38,9 @@ public class ProgramEntityConverter
 				.title(source.getTitle())
 				.content(source.getContent())
 				.programDate(source.getProgramDate())
+				.programCategory(source.getProgramCategory())
+				.programType(source.getProgramType())
+				.writer(source.getWriter())
 				.build();
 	}
 }
