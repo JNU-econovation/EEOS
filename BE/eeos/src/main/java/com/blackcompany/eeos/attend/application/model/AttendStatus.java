e87113384ed0e3047080df70491930115b829070
kssumin <201566@jnu.ac.kr>
2024-01-05 23:05:44 +0900

    chore : 공개되어 있는 메서드, enum에 주석 추가

--- BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/model/AttendStatus.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/model/AttendStatus.java
@@ -4,8 +4,17 @@ import com.blackcompany.eeos.attend.application.exception.NotFoundAttendStatusEx
 import java.util.Arrays;
 
 public enum AttendStatus {
+
+	/** 관련있음, 참석 */
 	ATTEND("attend"),
+	/** 관련있음, 불참 */
 	ABSENT("absent"),
+	/** 관련있음, 지각 */
+	LATE("late"),
+	/** 관련있음, 미응답 */
+	NONRESPONSE("nonResponse"),
+	/** 관련없음 */
+	NONRELATED("nonRelated"),
 	NONE("none");
 
 	private final String status;
--- BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/service/CandidateService.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/service/CandidateService.java
@@ -1,5 +1,9 @@
 package com.blackcompany.eeos.attend.application.service;
 
+import com.blackcompany.eeos.program.application.dto.ProgramMembers;
+import java.util.List;
+
+/** 관련 있는 대상자를 선택한다. */
 public interface CandidateService {
-	void saveCandidate(final Long programId);
+	void saveCandidate(final Long programId, final List<ProgramMembers> members);
 }
