dde2dea0942944258e13dafe201dbbedff6f1517
kssumin <201566@jnu.ac.kr>
2024-01-07 03:10:16 +0900

    refactor : 요청받은 멤버 중에 존재하지 않는 멤버가 있을 경우 에러처리

--- BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/service/SelectCandidateService.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/service/SelectCandidateService.java
@@ -3,17 +3,20 @@ package com.blackcompany.eeos.attend.application.service;
 import com.blackcompany.eeos.attend.application.model.converter.AttendEntityConverter;
 import com.blackcompany.eeos.attend.persistence.AttendEntity;
 import com.blackcompany.eeos.attend.persistence.AttendRepository;
+import com.blackcompany.eeos.member.application.exception.NotFoundMemberException;
 import com.blackcompany.eeos.member.persistence.MemberEntity;
 import com.blackcompany.eeos.member.persistence.MemberRepository;
 import com.blackcompany.eeos.program.application.dto.ProgramMembers;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /** 관련있는 대상자를 지정하여 선택한다. */
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SelectCandidateService implements CandidateService {
 	private final AttendRepository attendRepository;
 	private final AttendEntityConverter entityConverter;
@@ -30,9 +33,28 @@ public class SelectCandidateService implements CandidateService {
 	}
 
 	private List<MemberEntity> findMembers(final List<ProgramMembers> members) {
-		List<Long> memberIds =
+		List<Long> requestMember =
 				members.stream().map(ProgramMembers::getMemberId).collect(Collectors.toList());
 
-		return memberRepository.findUsersByIds(memberIds);
+		List<MemberEntity> findMembers = memberRepository.findUsersByIds(requestMember);
+		validateMember(requestMember, findMembers);
+		return findMembers;
+	}
+
+	private void validateMember(List<Long> requestMember, List<MemberEntity> findMembers) {
+		if (requestMember.size() == findMembers.size()) {
+			return;
+		}
+		log.error(String.valueOf(findDifferent(requestMember, findMembers)));
+		throw new NotFoundMemberException();
+	}
+
+	private List<Long> findDifferent(List<Long> requestMember, List<MemberEntity> memberEntities) {
+		List<Long> savedMember =
+				memberEntities.stream().map(MemberEntity::getId).collect(Collectors.toList());
+
+		requestMember.removeAll(savedMember);
+
+		return requestMember;
 	}
 }
