5a6d7a893fca71c8fe6e72cead4add3c760066e3
bada308 <203190@jnu.ac.kr>
2024-01-07 19:29:29 +0900

    feat: Login 페이지 레이아웃 CSS 변경

--- FE/src/app/(auth)/login/page.tsx
+++ FE/src/app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import LoginRightSection from "@/components/login/RightSection";
 
 const LoginPage = () => {
   return (
-    <div className="grid h-[38rem] grid-cols-[25rem_45rem] shadow-lg">
+    <div className="grid h-[44rem] grid-cols-[25rem_45rem] shadow-lg">
       <LoginLeftSection />
       <LoginRightSection />
     </div>
