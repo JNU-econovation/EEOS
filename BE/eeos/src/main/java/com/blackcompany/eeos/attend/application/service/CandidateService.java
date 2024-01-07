2ccb1e45da311fe67321083592208ecd3c520e22
kssumin <201566@jnu.ac.kr>
2024-01-07 13:22:55 +0900

    feat : 프로그램의 참여 대상자 및 참여 정보 변경 구현

--- BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/service/CandidateService.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/service/CandidateService.java
@@ -1,9 +1,12 @@
 package com.blackcompany.eeos.attend.application.service;
 
+import com.blackcompany.eeos.program.application.dto.ChangeAttendStatusRequest;
 import com.blackcompany.eeos.program.application.dto.ProgramMembers;
 import java.util.List;
 
 /** 관련 있는 대상자를 선택한다. */
 public interface CandidateService {
 	void saveCandidate(final Long programId, final List<ProgramMembers> members);
+
+	void updateCandidate(final Long programId, final List<ChangeAttendStatusRequest> requests);
 }
--- BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/service/SelectCandidateService.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/service/SelectCandidateService.java
@@ -1,17 +1,25 @@
 package com.blackcompany.eeos.attend.application.service;
 
+import com.blackcompany.eeos.attend.application.model.AttendModel;
+import com.blackcompany.eeos.attend.application.model.AttendStatus;
 import com.blackcompany.eeos.attend.application.model.converter.AttendEntityConverter;
 import com.blackcompany.eeos.attend.persistence.AttendEntity;
 import com.blackcompany.eeos.attend.persistence.AttendRepository;
+import com.blackcompany.eeos.common.persistence.MemberIdEntity;
 import com.blackcompany.eeos.member.application.exception.NotFoundMemberException;
 import com.blackcompany.eeos.member.persistence.MemberEntity;
 import com.blackcompany.eeos.member.persistence.MemberRepository;
+import com.blackcompany.eeos.program.application.dto.ChangeAttendStatusRequest;
 import com.blackcompany.eeos.program.application.dto.ProgramMembers;
+import com.blackcompany.eeos.program.application.model.AttendManager;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /** 관련있는 대상자를 지정하여 선택한다. */
 @Service
@@ -23,6 +31,7 @@ public class SelectCandidateService implements CandidateService {
 	private final MemberRepository memberRepository;
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
 	public void saveCandidate(final Long programId, final List<ProgramMembers> members) {
 		List<AttendEntity> attendEntities =
 				findMembers(members).stream()
@@ -32,29 +41,102 @@ public class SelectCandidateService implements CandidateService {
 		attendRepository.saveAll(attendEntities);
 	}
 
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public void updateCandidate(
+			final Long programId, final List<ChangeAttendStatusRequest> requests) {
+		List<AttendModel> models =
+				findAttends(programId, requests).stream()
+						.map(entityConverter::from)
+						.collect(Collectors.toList());
+
+		AttendManager attendManager = new AttendManager();
+		models.forEach(model -> updateAttendStatus(model, requests, attendManager));
+
+		deleteNonRelated(attendManager);
+		updateRelated(attendManager);
+	}
+
 	private List<MemberEntity> findMembers(final List<ProgramMembers> members) {
 		List<Long> requestMember =
 				members.stream().map(ProgramMembers::getMemberId).collect(Collectors.toList());
 
 		List<MemberEntity> findMembers = memberRepository.findUsersByIds(requestMember);
-		validateMember(requestMember, findMembers);
+		validateAllFind(requestMember, findMembers);
 		return findMembers;
 	}
 
-	private void validateMember(List<Long> requestMember, List<MemberEntity> findMembers) {
-		if (requestMember.size() == findMembers.size()) {
+	private List<AttendEntity> findAttends(
+			final Long programId, final List<ChangeAttendStatusRequest> members) {
+		List<Long> memberIds =
+				members.stream().map(ChangeAttendStatusRequest::getMemberId).collect(Collectors.toList());
+
+		List<AttendEntity> attendMembers =
+				attendRepository.findAllByProgramMember(programId, memberIds);
+		validateAllFind(memberIds, attendMembers);
+		return attendMembers;
+	}
+
+	private <T extends MemberIdEntity> void validateAllFind(
+			List<Long> requestEntities, List<T> findEntities) {
+		if (requestEntities.size() == findEntities.size()) {
 			return;
 		}
-		log.error(String.valueOf(findDifferent(requestMember, findMembers)));
+		log.error(String.valueOf(findDifferent(requestEntities, findEntities)));
 		throw new NotFoundMemberException();
 	}
 
-	private List<Long> findDifferent(List<Long> requestMember, List<MemberEntity> memberEntities) {
-		List<Long> savedMember =
-				memberEntities.stream().map(MemberEntity::getId).collect(Collectors.toList());
+	private <T extends MemberIdEntity> List<Long> findDifferent(
+			List<Long> requestEntities, List<T> findEntities) {
+		List<Long> findEntitiesId =
+				findEntities.stream().map(MemberIdEntity::getId).collect(Collectors.toList());
 
-		requestMember.removeAll(savedMember);
+		requestEntities.removeAll(findEntitiesId);
 
-		return requestMember;
+		return requestEntities;
+	}
+
+	private void updateAttendStatus(
+			AttendModel model, List<ChangeAttendStatusRequest> requests, AttendManager attendManager) {
+		ChangeAttendStatusRequest request = findUpdateRequest(model.getMemberId(), requests);
+
+		validateSameBeforeStatus(model, request);
+
+		if (Objects.equals(request.getAfterAttendStatus(), AttendStatus.NONRELATED.getStatus())) {
+			attendManager.addNonRelated(model);
+			return;
+		}
+		model.changeStatus(request.getAfterAttendStatus());
+		attendManager.addRelated(model);
+	}
+
+	private ChangeAttendStatusRequest findUpdateRequest(
+			Long attendId, List<ChangeAttendStatusRequest> requests) {
+		return requests.stream()
+				.filter(request -> request.getMemberId().equals(attendId))
+				.findAny()
+				.orElseThrow(NotFoundMemberException::new);
+	}
+
+	private void validateSameBeforeStatus(AttendModel model, ChangeAttendStatusRequest request) {
+		model.validateSame(request.getBeforeAttendStatus());
+	}
+
+	private void deleteNonRelated(AttendManager attendManager) {
+		List<AttendEntity> nonRelated =
+				attendManager.getNonRelated().stream()
+						.map(entityConverter::toEntity)
+						.collect(Collectors.toList());
+
+		attendRepository.deleteAll(nonRelated);
+	}
+
+	private void updateRelated(AttendManager attendManager) {
+		List<AttendEntity> related =
+				attendManager.getRelated().stream()
+						.map(entityConverter::toEntity)
+						.collect(Collectors.toList());
+
+		attendRepository.saveAll(related);
 	}
 }
