b4423ec4f486a82d596d88c733ba9d749bf14744
kssumin <201566@jnu.ac.kr>
2024-01-07 13:03:33 +0900

    feat : 관련없음으로 인한 삭제 대상자, 참석 정보 수정 대상자를 관리하는 AttendManager

---
+++ BE/eeos/src/main/java/com/blackcompany/eeos/program/application/model/AttendManager.java
@@ -0,0 +1,27 @@
+package com.blackcompany.eeos.program.application.model;
+
+import com.blackcompany.eeos.attend.application.model.AttendModel;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AttendManager {
+
+	private final List<AttendModel> nonRelated = new ArrayList<>();
+	private final List<AttendModel> related = new ArrayList<>();
+
+	public void addRelated(AttendModel model) {
+		related.add(model);
+	}
+
+	public void addNonRelated(AttendModel model) {
+		nonRelated.add(model);
+	}
+
+	public List<AttendModel> getNonRelated() {
+		return List.copyOf(nonRelated);
+	}
+
+	public List<AttendModel> getRelated() {
+		return List.copyOf(related);
+	}
+}
