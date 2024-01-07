b47c7a201feba0f916e7cbdc95cc950a66e3b399
kssumin <201566@jnu.ac.kr>
2024-01-07 16:19:41 +0900

    feat : 본인의 참석 정보 변경 api 구현

--- BE/eeos/src/main/java/com/blackcompany/eeos/attend/presentation/AttendController.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/attend/presentation/AttendController.java
@@ -1,19 +1,20 @@
 package com.blackcompany.eeos.attend.presentation;
 
 import com.blackcompany.eeos.attend.application.dto.AttendInfoResponse;
+import com.blackcompany.eeos.attend.application.dto.ChangeAttendStatusRequest;
 import com.blackcompany.eeos.attend.application.usecase.ChangeAttendStatusUsecase;
 import com.blackcompany.eeos.attend.application.usecase.GetAttendantInfoUsecase;
+import com.blackcompany.eeos.auth.presentation.support.Member;
 import com.blackcompany.eeos.common.presentation.respnose.ApiResponse;
 import com.blackcompany.eeos.common.presentation.respnose.ApiResponseBody.SuccessBody;
 import com.blackcompany.eeos.common.presentation.respnose.ApiResponseGenerator;
 import com.blackcompany.eeos.common.presentation.respnose.MessageCode;
-import com.blackcompany.eeos.program.application.dto.ChangeAttendStatusRequest;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,10 +35,12 @@ public class AttendController {
 		return ApiResponseGenerator.success(response, HttpStatus.OK, MessageCode.GET);
 	}
 
-	@PostMapping("/programs/{programId}")
+	@PutMapping("/programs/{programId}")
 	public ApiResponse<SuccessBody<Void>> changeAttendStatus(
-			@PathVariable("programId") Long programId, @RequestBody ChangeAttendStatusRequest request) {
-		changeAttendStatusUsecase.changeStatus(request, programId);
+			@Member Long memberId,
+			@PathVariable("programId") Long programId,
+			@RequestBody ChangeAttendStatusRequest request) {
+		changeAttendStatusUsecase.changeStatus(memberId, request, programId);
 		return ApiResponseGenerator.success(HttpStatus.OK, MessageCode.UPDATE);
 	}
 
