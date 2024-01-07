6cc24099d6e99c2abe342691c254313631358256
kssumin <201566@jnu.ac.kr>
2024-01-07 12:59:21 +0900

    feat : 프로그램 편집 권한 없을 때 발행하는 예외 구현

--- BE/eeos/src/main/java/com/blackcompany/eeos/program/application/exception/DeniedProgramEditException.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/program/application/exception/DeniedProgramEditException.java
@@ -4,17 +4,17 @@ import com.blackcompany.eeos.common.exception.BusinessException;
 import org.springframework.http.HttpStatus;
 
 /** 해당 프로그램에 편집(수정, 삭제) 권한이 없을 때 발생하는 예외 */
-public class ProgramEditDeniedException extends BusinessException {
+public class DeniedProgramEditException extends BusinessException {
 	private static final String FAIL_CODE = "1005";
 	private final Long programId;
 
-	public ProgramEditDeniedException(Long programId) {
+	public DeniedProgramEditException(Long programId) {
 		super(FAIL_CODE, HttpStatus.NOT_FOUND);
 		this.programId = programId;
 	}
 
 	@Override
 	public String getMessage() {
-		return String.format("% 프로그램 편집 권한(수정/삭제)이 없는 사용자입니다.", programId);
+		return String.format("%s 프로그램 편집 권한(수정/삭제)이 없는 사용자입니다.", programId);
 	}
 }
