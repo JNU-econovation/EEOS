bcf0e597d2726325ffed159fc9c81daa863ac793
bada308 <203190@jnu.ac.kr>
2024-01-07 19:28:32 +0900

    feat: deleteTokenInfo 유틸 함수 사용

--- FE/src/components/common/Validate.tsx
+++ FE/src/components/common/Validate.tsx
@@ -1,17 +1,19 @@
 "use client";
 
-import { useDeleteTokenAndRedirect } from "@/hooks/useDeleteTokenAndRedirect";
+import ROUTES from "@/constants/ROUTES";
+import { deleteTokenInfo } from "@/utils/authWithStorage";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const Validate = () => {
-  const deleteTokenAndRedirect = useDeleteTokenAndRedirect();
-
+  const router = useRouter();
   useEffect(() => {
     const accessToken = localStorage.getItem("accessToken");
     const tokenExpiration = localStorage.getItem("tokenExpiration");
     console.log(accessToken, tokenExpiration);
     if (!accessToken || !tokenExpiration) {
-      deleteTokenAndRedirect();
+      deleteTokenInfo();
+      router.push(ROUTES.LOGIN);
     }
   }, []);
 
