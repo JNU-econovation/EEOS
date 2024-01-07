e6c841e59bac5f6d48c3e571638d807d09e111af
kssumin <201566@jnu.ac.kr>
2024-01-07 15:25:00 +0900

    chore : 프로그램 detail 정보 가져오기 usecase 주석 추가

--- BE/eeos/src/main/java/com/blackcompany/eeos/program/application/usecase/GetProgramUsecase.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/program/application/usecase/GetProgramUsecase.java
@@ -3,5 +3,14 @@ package com.blackcompany.eeos.program.application.usecase;
 import com.blackcompany.eeos.program.application.dto.QueryProgramResponse;
 
 public interface GetProgramUsecase {
+	/**
+	 * 프로그램 detail 정보를 가져온다.
+	 *
+	 * <p>이때 프로그램 접근 권한도 같이 반환한다.
+	 *
+	 * @param memberId 접근한 유저의 정보
+	 * @param programId 프로그램 정보
+	 * @return
+	 */
 	QueryProgramResponse getProgram(Long memberId, Long programId);
 }
