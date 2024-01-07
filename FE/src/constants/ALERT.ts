0fe29922854003b06763d3555fc3f15779949437
bada308 <203190@jnu.ac.kr>
2024-01-07 18:10:17 +0900

    feat: Auth 관련 Alert 메세지 추가

--- FE/src/constants/ALERT.ts
+++ FE/src/constants/ALERT.ts
@@ -2,5 +2,12 @@ const EDIT_DISABLED = {
   PROGRAM_ACTIVE: "진행중인 프로그램은 수정할 수 없습니다.",
 };
 
+const AUTH = {
+  LOGIN_REQUIRED: "로그인이 필요합니다.",
+  LOGIN_FAILED: "로그인에 실패했습니다.",
+};
+
 Object.freeze(EDIT_DISABLED);
-export default { EDIT_DISABLED };
+Object.freeze(AUTH);
+
+export default { EDIT_DISABLED, AUTH };
