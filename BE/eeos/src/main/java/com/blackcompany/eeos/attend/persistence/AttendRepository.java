1d5f03a3dcc06f56818ddb81b3f3403c61011990
kssumin <201566@jnu.ac.kr>
2024-01-07 13:02:45 +0900

    feat : 프로그램, 요청한 멤버 정보와 일치하는 참석 정보 불러오는 쿼리 구현

--- BE/eeos/src/main/java/com/blackcompany/eeos/attend/persistence/AttendRepository.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/attend/persistence/AttendRepository.java
@@ -4,6 +4,8 @@ import com.blackcompany.eeos.attend.application.model.AttendStatus;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AttendRepository extends JpaRepository<AttendEntity, Long> {
 	List<AttendEntity> findAllByProgramIdAndStatus(Long programId, AttendStatus status);
@@ -11,4 +13,9 @@ public interface AttendRepository extends JpaRepository<AttendEntity, Long> {
 	Optional<AttendEntity> findByProgramIdAndMemberId(Long programId, Long memberId);
 
 	void deleteAllByProgramId(Long programId);
+
+	@Query(
+			"SELECT a FROM AttendEntity  a WHERE a.programId =:programId AND a.id IN :ids AND a.isDeleted=false")
+	List<AttendEntity> findAllByProgramMember(
+			@Param("programId") Long programId, @Param("ids") List<Long> memberIds);
 }
