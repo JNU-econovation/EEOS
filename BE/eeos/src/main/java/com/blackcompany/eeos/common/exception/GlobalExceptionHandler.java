e2a15052e42eeabbe6670c925e674197bbdbb94d
kssumin <201566@jnu.ac.kr>
2024-01-08 23:40:58 +0900

    fix : custom error 코드 body에 추가

--- BE/eeos/src/main/java/com/blackcompany/eeos/common/exception/GlobalExceptionHandler.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/common/exception/GlobalExceptionHandler.java
@@ -20,7 +20,8 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(BindException.class)
 	protected ApiResponse<FailureBody> handleBindException(BindException e) {
 		log.error("handleBindException", e);
-		return ApiResponseGenerator.fail(e.getBindingResult(), HttpStatus.BAD_REQUEST);
+		String code = String.valueOf(HttpStatus.BAD_REQUEST.value());
+		return ApiResponseGenerator.fail(e.getBindingResult(), code, HttpStatus.BAD_REQUEST);
 	}
 
 	/** 주로 @RequestParam enum으로 binding 못했을 경우 발생 */
@@ -28,7 +29,8 @@ public class GlobalExceptionHandler {
 	protected ApiResponse<FailureBody> handleMethodArgumentTypeMismatchException(
 			MethodArgumentTypeMismatchException e) {
 		log.error("handleMethodArgumentTypeMismatchException", e);
-		return ApiResponseGenerator.fail(e.getMessage(), HttpStatus.BAD_REQUEST);
+		String code = String.valueOf(HttpStatus.BAD_REQUEST.value());
+		return ApiResponseGenerator.fail(e.getMessage(), code, HttpStatus.BAD_REQUEST);
 	}
 
 	/** 지원하지 않은 HTTP method 호출 할 경우 발생 */
@@ -36,27 +38,32 @@ public class GlobalExceptionHandler {
 	protected ApiResponse<FailureBody> handleHttpRequestMethodNotSupportedException(
 			HttpRequestMethodNotSupportedException e) {
 		log.error("handleHttpRequestMethodNotSupportedException", e);
-		return ApiResponseGenerator.fail(e.getMessage(), HttpStatus.METHOD_NOT_ALLOWED);
+		String code = String.valueOf(HttpStatus.METHOD_NOT_ALLOWED.value());
+		return ApiResponseGenerator.fail(e.getMessage(), code, HttpStatus.METHOD_NOT_ALLOWED);
 	}
 
 	/** 비즈니스 로직 실행 중 오류 발생 */
 	@ExceptionHandler(value = {BusinessException.class})
 	protected ApiResponse<FailureBody> handleConflict(BusinessException e) {
 		log.error("BusinessException", e);
-		return ApiResponseGenerator.fail(e.getMessage(), e.getHttpStatus());
+		return ApiResponseGenerator.fail(e.getMessage(), e.getCode(), e.getHttpStatus());
 	}
 
 	/** 나머지 예외 발생 */
 	@ExceptionHandler(Exception.class)
 	protected ApiResponse<FailureBody> handleException(Exception e) {
 		log.error("Exception", e);
-		return ApiResponseGenerator.fail(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		String code = String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		return ApiResponseGenerator.fail(e.getMessage(), code, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	@ExceptionHandler({MethodArgumentNotValidException.class})
 	protected ApiResponse<FailureBody> handleMethodArgumentNotValidException(
 			MethodArgumentNotValidException e) {
+		String code = String.valueOf(HttpStatus.BAD_REQUEST.value());
 		return ApiResponseGenerator.fail(
-				e.getBindingResult().getFieldErrors().get(0).getDefaultMessage(), HttpStatus.BAD_REQUEST);
+				e.getBindingResult().getFieldErrors().get(0).getDefaultMessage(),
+				code,
+				HttpStatus.BAD_REQUEST);
 	}
 }
