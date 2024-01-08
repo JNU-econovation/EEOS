644083b9b79a0ca4fa2090352cf4015009c118b8
bada308 <203190@jnu.ac.kr>
2024-01-08 17:42:52 +0900

    feat: Title 컴포넌트에 color props 추가

--- FE/src/components/common/Title.tsx
+++ FE/src/components/common/Title.tsx
@@ -1,8 +1,18 @@
+import classNames from "classnames";
+
 interface TitleProps {
   text: string;
+  textColor?: string;
 }
 
-const Title = ({ text }: TitleProps) => {
-  return <h1 className="text-3xl font-bold text-black">{text}</h1>;
+const titleColors = {
+  black: "text-black",
+  white: "text-background",
+  error: "text-error",
+};
+
+const Title = ({ text, textColor = "black" }: TitleProps) => {
+  const titleStyle = classNames("text-3xl font-bold", titleColors[textColor]);
+  return <h1 className={titleStyle}>{text}</h1>;
 };
 export default Title;
