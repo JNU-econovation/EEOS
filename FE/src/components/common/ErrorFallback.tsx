928adaeefe32f6b5121f975e6fdef987d5a76918
bada308 <203190@jnu.ac.kr>
2024-01-08 20:39:34 +0900

    feat: ErrorFallback component to use react-query error reset

--- FE/src/components/common/ErrorFallback.tsx
+++ FE/src/components/common/ErrorFallback.tsx
@@ -1,28 +1,36 @@
 import Image from "next/image";
-import { FallbackProps } from "react-error-boundary";
 import Title from "./Title";
 import Button from "./Button";
+import { useQueryErrorResetBoundary } from "@tanstack/react-query";
 
 const ERROR_TITLE = "ERROR";
 const RETRY_BUTTON_TEXT = "Try again";
 
-const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
+type FallbackProps = {
+  error: any;
+  resetErrorBoundary?: (...args: any[]) => void;
+};
+
+const ErrorFallback = ({
+  error,
+  resetErrorBoundary = () => {},
+}: FallbackProps) => {
+  const { reset } = useQueryErrorResetBoundary();
+  const handleReset = () => {
+    reset();
+    resetErrorBoundary();
+  };
   return (
-    <div className="flex h-full w-full flex-col gap-6 bg-background px-28 py-12">
+    <div className="flex h-fit w-full min-w-[15rem] flex-col items-center justify-center gap-4 bg-background py-6">
       <Image
         src="/icons/error.svg"
         alt="에러 바운더리"
-        width={80}
-        height={80}
+        width={60}
+        height={60}
       />
       <Title text={ERROR_TITLE} textColor="error" />
-      <p className="text-sm font-normal">{error.messaeg}</p>
-      <Button
-        color="error"
-        size="lg"
-        type="button"
-        onClick={() => resetErrorBoundary()}
-      >
+      {error?.message && <p className="text-sm font-normal">{error.message}</p>}
+      <Button color="error" size="lg" type="button" onClick={handleReset}>
         {RETRY_BUTTON_TEXT}
       </Button>
     </div>
