9d1ecd39c0b679e9e7f0f2cdcc548460ae8aa443
bada308 <203190@jnu.ac.kr>
2024-01-08 21:33:03 +0900

    feat: icon 없는 ErrorFallback 컴포넌트 구현

---
+++ FE/src/components/common/ErrorFallbackNoIcon.tsx
@@ -0,0 +1,32 @@
+import Title from "./Title";
+import Button from "./Button";
+import { useQueryErrorResetBoundary } from "@tanstack/react-query";
+
+const ERROR_TITLE = "ERROR";
+const RETRY_BUTTON_TEXT = "Try again";
+
+type FallbackProps = {
+  error: any;
+  resetErrorBoundary?: (...args: any[]) => void;
+};
+
+const ErrorFallbackNoIcon = ({
+  error,
+  resetErrorBoundary = () => {},
+}: FallbackProps) => {
+  const { reset } = useQueryErrorResetBoundary();
+  const handleReset = () => {
+    reset();
+    resetErrorBoundary();
+  };
+  return (
+    <div className="flex h-fit w-full min-w-[15rem] flex-col items-center justify-center gap-5 bg-background py-6">
+      <Title text={ERROR_TITLE} textColor="error" />
+      {error?.message && <p className="text-sm font-normal">{error.message}</p>}
+      <Button color="error" size="lg" type="button" onClick={handleReset}>
+        {RETRY_BUTTON_TEXT}
+      </Button>
+    </div>
+  );
+};
+export default ErrorFallbackNoIcon;
