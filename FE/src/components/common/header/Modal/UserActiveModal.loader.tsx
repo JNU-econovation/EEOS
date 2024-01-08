18c9561ec176229d602cd677542b14e651758288
bada308 <203190@jnu.ac.kr>
2024-01-08 20:44:27 +0900

    feat: UserActiveModal 로더 레이아웃 수정

--- FE/src/components/common/header/Modal/UserActiveModal.loader.tsx
+++ FE/src/components/common/header/Modal/UserActiveModal.loader.tsx
@@ -2,9 +2,9 @@ import LoadingSpinner from "../../LoadingSpinner";
 
 const UserActiveModalSkeleton = () => {
   return (
-    <section className="absolute -left-32 top-10 flex h-80 w-80 flex-col items-center gap-6 rounded-2xl bg-background px-12 py-6 drop-shadow-lg">
+    <div className="h-40 w-full">
       <LoadingSpinner />
-    </section>
+    </div>
   );
 };
 
