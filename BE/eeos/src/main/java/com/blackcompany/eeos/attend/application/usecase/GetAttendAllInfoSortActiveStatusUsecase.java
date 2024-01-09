c26298344089557a817cf908b1f301e8100da104
kssumin <201566@jnu.ac.kr>
2024-01-09 14:46:22 +0900

    refactor : 참석 정보 관련 service 메서드명 변경

--- BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/usecase/GetAttendAllInfoSortActiveStatusUsecase.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/usecase/GetAttendAllInfoSortActiveStatusUsecase.java
@@ -2,6 +2,7 @@ package com.blackcompany.eeos.attend.application.usecase;
 
 import com.blackcompany.eeos.attend.application.dto.QueryAttendActiveStatusResponse;
 
+/** 프로그램에 관련 없음 대상자도 활동 상태를 기준으로 조회한다. */
 public interface GetAttendAllInfoSortActiveStatusUsecase {
-	QueryAttendActiveStatusResponse execute(final Long programId, final String activeStatus);
+	QueryAttendActiveStatusResponse getAttendInfo(final Long programId, final String activeStatus);
 }
--- BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/usecase/GetAttendantInfoUsecase.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/attend/application/usecase/GetAttendantInfoUsecase.java
@@ -7,5 +7,12 @@ import java.util.List;
 public interface GetAttendantInfoUsecase {
 	List<AttendInfoResponse> findAttendInfo(final Long programId);
 
+	/**
+	 * 프로그램의 관련 있음 대상자만 참석 상태를 기준으로 조회한다.
+	 *
+	 * @param programId
+	 * @param attendStatus
+	 * @return
+	 */
 	QueryAttendStatusResponse findAttendInfo(final Long programId, final String attendStatus);
 }
