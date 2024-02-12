cb40b60cf288d54be18796d3a3afc5bfb515beeb
bada308 <203190@jnu.ac.kr>
2024-02-12 15:05:55 +0900

    feat: 전체 레이아웃 수정

--- FE/src/app/(private)/(program)/layout.tsx
+++ FE/src/app/(private)/(program)/layout.tsx
@@ -5,7 +5,7 @@ export default function ProgramLayout({ children }: PropsWithChildren<{}>) {
   return (
     <>
       <Header />
-      <main className="mb-28 mt-16 h-full w-full max-w-[500px] sm:max-w-[800px] lg:max-w-[1112px]">
+      <main className="mb-28 mt-16 h-full w-full px-3 sm:max-w-[800px] lg:max-w-[1112px]">
         {children}
       </main>
     </>
