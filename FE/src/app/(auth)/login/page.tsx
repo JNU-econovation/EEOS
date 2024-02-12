a874b3944b83599ea1e911ec4767f6f67e2b4106
bada308 <203190@jnu.ac.kr>
2024-02-12 14:29:45 +0900

    feat: 로그인 페이지 반응형 적용

--- FE/src/app/(auth)/login/page.tsx
+++ FE/src/app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import LoginRightSection from "@/components/login/RightSection";
 
 const LoginPage = () => {
   return (
-    <div className="grid h-[44rem] grid-cols-[25rem_45rem] shadow-lg">
+    <div className="grid h-[80vh] sm:h-[44rem] sm:grid-cols-[25rem_1fr] sm:shadow-lg">
       <LoginLeftSection />
       <LoginRightSection />
     </div>
