895222309e63c394452df7beb391d667bf8569d2
bada308 <203190@jnu.ac.kr>
2024-02-12 16:02:56 +0900

    feat: 화면 크기에 따라 gap 크기 수정

--- FE/src/components/common/header/Header.tsx
+++ FE/src/components/common/header/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="sticky top-0 z-50 flex w-full items-center justify-between rounded-b-xl bg-background px-2 py-4 shadow-sm sm:px-32">
       <Logo />
-      <section className="flex w-fit items-center gap-8">
+      <section className="flex w-fit items-center gap-4 sm:gap-8">
         <UserBtn />
         <CreateBtn />
       </section>
