fb2a784019bc98cd192602cccca719f029426a7a
bada308 <203190@jnu.ac.kr>
2024-02-12 16:44:04 +0900

    feat: member list 반응형 수정

--- FE/src/components/programDetail/attendee/MemberList.tsx
+++ FE/src/components/programDetail/attendee/MemberList.tsx
@@ -7,7 +7,7 @@ interface MemberListProps {
 
 const MemberList = ({ members }: MemberListProps) => {
   return (
-    <div className="grid w-full auto-cols-auto justify-items-center gap-x-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div className="grid w-full auto-cols-auto grid-cols-2 justify-items-center gap-x-4 md:grid-cols-3 lg:grid-cols-4">
       {members.map((member) => (
         <MemberListItem key={member.memberId} memberData={member} />
       ))}
