388d41b89bb361ad78530e731613d27052392695
bada308 <203190@jnu.ac.kr>
2024-02-14 19:34:30 +0900

    feat: 입력 상태 관련된 상수 선언

---
+++ FE/src/constants/INPUT_STATUS.ts
@@ -0,0 +1,43 @@
+import { TabOption } from "@/types/tab";
+
+export type InputStatus = "incomplete" | "complete";
+
+export interface InputStatusToggleOption extends TabOption<InputStatus> {
+  color: string;
+}
+
+type InputStatusToggle = {
+  [key in InputStatus]: InputStatusToggleOption;
+};
+
+type InputStatusList = {
+  [key in InputStatus]: TabOption<InputStatus> & {
+    icon: string;
+    color: string;
+  };
+};
+
+const TOGGLE: InputStatusToggle = {
+  incomplete: { type: "incomplete", text: "입력해주세요!", color: "teal" },
+  complete: { type: "complete", text: "입력 완료", color: "green" },
+};
+
+const LIST: InputStatusList = {
+  complete: {
+    type: "complete",
+    text: "입력",
+    icon: "/icons/check.svg",
+    color: "green",
+  },
+  incomplete: {
+    type: "incomplete",
+    text: "미입력",
+    icon: "/icons/minus.svg",
+    color: "gray",
+  },
+};
+
+Object.freeze(TOGGLE);
+Object.freeze(LIST);
+
+export default { TOGGLE, LIST };
