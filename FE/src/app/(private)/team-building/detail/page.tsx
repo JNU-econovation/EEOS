c6dde56b05df6de4843150d18d911e9b82ec86b4
bada308 <203190@jnu.ac.kr>
2024-02-14 20:21:22 +0900

    feat: 팀빌딩 세부 페이지 구현

---
+++ FE/src/app/(private)/team-building/detail/page.tsx
@@ -0,0 +1,15 @@
+import InputStatusInfoContainer from "@/components/teamBuildingDetail/inputStatus/InputStatusInfo.container";
+import TeamBuildingInfo from "@/components/teamBuildingDetail/teamBuilding/TeamBuildingInfo";
+import UserInputModalContainer from "@/components/teamBuildingDetail/userInputModal/UserInputModal.container";
+
+const TeamBuildingDetailPage = () => {
+  return (
+    <div className="mb-16 space-y-16">
+      <TeamBuildingInfo />
+      <InputStatusInfoContainer />
+      <UserInputModalContainer />
+    </div>
+  );
+};
+
+export default TeamBuildingDetailPage;
