9700dd402f6d11fd59e953429508b85d21c5d8a4
kssumin <201566@jnu.ac.kr>
2024-02-15 15:18:09 +0900

    feat : 팀빌딩 저장 서비스 로직 구현

---
+++ BE/eeos/src/main/java/com/blackcompany/eeos/teamBuilding/application/service/CommandTeamBuildingService.java
@@ -0,0 +1,29 @@
+package com.blackcompany.eeos.teamBuilding.application.service;
+
+import com.blackcompany.eeos.attend.application.service.SelectTeamBuildingTargetService;
+import com.blackcompany.eeos.teamBuilding.application.dto.CreateTeamBuildingRequest;
+import com.blackcompany.eeos.teamBuilding.application.model.TeamBuildingModel;
+import com.blackcompany.eeos.teamBuilding.application.model.converter.TeamBuildingEntityConverter;
+import com.blackcompany.eeos.teamBuilding.application.model.converter.TeamBuildingRequestConverter;
+import com.blackcompany.eeos.teamBuilding.application.usecase.CreateTeamBuildingUsecase;
+import com.blackcompany.eeos.teamBuilding.persistence.TeamBuildingEntity;
+import com.blackcompany.eeos.teamBuilding.persistence.TeamBuildingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommandTeamBuildingService implements CreateTeamBuildingUsecase {
+	private final TeamBuildingRequestConverter requestConverter;
+	private final TeamBuildingEntityConverter entityConverter;
+	private final TeamBuildingRepository repository;
+	private final SelectTeamBuildingTargetService teamBuildingTargetService;
+
+	@Override
+	public void create(Long memberId, CreateTeamBuildingRequest request) {
+		TeamBuildingModel model = requestConverter.from(request);
+		TeamBuildingEntity savedEntity = repository.save(entityConverter.toEntity(model));
+
+		teamBuildingTargetService.save(savedEntity.getId(), request.getMembers());
+	}
+}
