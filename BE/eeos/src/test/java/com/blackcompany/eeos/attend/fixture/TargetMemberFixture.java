8c19ef04f526005a823c3109bdf3dfbc8386c551
kssumin <201566@jnu.ac.kr>
2024-02-15 16:35:38 +0900

    feat : 대상자를 전달받아 참석 대상자로 저장하는 로직 테스트

--- BE/eeos/src/test/java/com/blackcompany/eeos/attend/fixture/TargetMemberFixture.java
+++ BE/eeos/src/test/java/com/blackcompany/eeos/attend/fixture/TargetMemberFixture.java
@@ -1,5 +1,6 @@
 package com.blackcompany.eeos.attend.fixture;
 
+import com.blackcompany.eeos.program.application.dto.ProgramMembers;
 import com.blackcompany.eeos.teamBuilding.application.dto.TeamBuildingMember;
 import java.util.List;
 
@@ -14,5 +15,17 @@ public class TargetMemberFixture {
 
 	public static TeamBuildingMember 바다_팀빌딩_대상자() {
 		return TeamBuildingMember.builder().memberId(2L).build();
+	}
+
+	public static List<ProgramMembers> 수민_바다_프로그램_대상자() {
+		return List.of(수민_프로그램_대상자(), 바다_프로그램_대상자());
+	}
+
+	public static ProgramMembers 수민_프로그램_대상자() {
+		return ProgramMembers.builder().memberId(1L).build();
+	}
+
+	public static ProgramMembers 바다_프로그램_대상자() {
+		return ProgramMembers.builder().memberId(2L).build();
 	}
 }
