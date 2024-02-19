07147ad57b6e4ac8a238477a8395d8f96afbfee6
kssumin <201566@jnu.ac.kr>
2024-02-19 21:24:02 +0900

    chore : 린트 적용

--- BE/eeos/src/main/java/com/blackcompany/eeos/teamBuilding/persistence/TeamBuildingRepository.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/teamBuilding/persistence/TeamBuildingRepository.java
@@ -4,6 +4,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamBuildingRepository extends JpaRepository<TeamBuildingEntity, Long> {
-
 	Optional<TeamBuildingEntity> findByStatus(TeamBuildingStatus status);
 }
