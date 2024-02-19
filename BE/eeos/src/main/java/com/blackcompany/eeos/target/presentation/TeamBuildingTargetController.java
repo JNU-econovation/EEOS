b02b263cf0ee8b88f436bcebe7e4743cc5122eb3
kssumin <201566@jnu.ac.kr>
2024-02-19 22:32:47 +0900

    feat : 팀빌딩 참여 입력 사항을 변경하는 api 구현

--- BE/eeos/src/main/java/com/blackcompany/eeos/target/presentation/TeamBuildingTargetController.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/target/presentation/TeamBuildingTargetController.java
@@ -7,9 +7,11 @@ import com.blackcompany.eeos.common.presentation.respnose.ApiResponseGenerator;
 import com.blackcompany.eeos.common.presentation.respnose.MessageCode;
 import com.blackcompany.eeos.target.application.dto.AttendTeamBuildingRequest;
 import com.blackcompany.eeos.target.application.usecase.AttendTeamBuildingUsecase;
+import com.blackcompany.eeos.target.application.usecase.UpdateAttendTeamBuildingUsecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TeamBuildingTargetController {
 	private final AttendTeamBuildingUsecase attendTeamBuildingUsecase;
+	private final UpdateAttendTeamBuildingUsecase updateAttendTeamBuildingUsecase;
 
 	@PostMapping("/target/team-building")
-	public ApiResponse<SuccessBody<Void>> attendTeamBuilding(
+	public ApiResponse<SuccessBody<Void>> create(
 			@Member Long memberId, @RequestBody AttendTeamBuildingRequest request) {
 
 		attendTeamBuildingUsecase.create(memberId, request);
 		return ApiResponseGenerator.success(HttpStatus.CREATED, MessageCode.CREATE);
+	}
+
+	@PutMapping("/target/team-building")
+	public ApiResponse<SuccessBody<Void>> update(
+			@Member Long memberId, @RequestBody AttendTeamBuildingRequest request) {
+
+		updateAttendTeamBuildingUsecase.update(memberId, request);
+		return ApiResponseGenerator.success(HttpStatus.OK, MessageCode.UPDATE);
 	}
 }
