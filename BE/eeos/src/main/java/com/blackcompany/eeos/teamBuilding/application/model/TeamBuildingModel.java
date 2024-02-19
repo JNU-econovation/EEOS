2240836404e4c035119d405a27a1ec885de13d66
kssumin <201566@jnu.ac.kr>
2024-02-19 22:32:01 +0900

    feat : 팀빌딩 입력 사항을 변경할 수 있는 팀빌딩 상태인지 검증

--- BE/eeos/src/main/java/com/blackcompany/eeos/teamBuilding/application/model/TeamBuildingModel.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/teamBuilding/application/model/TeamBuildingModel.java
@@ -1,7 +1,9 @@
 package com.blackcompany.eeos.teamBuilding.application.model;
 
 import com.blackcompany.eeos.common.support.AbstractModel;
+import com.blackcompany.eeos.target.application.exception.DeniedSaveAttendTeamBuildingException;
 import com.blackcompany.eeos.teamBuilding.application.exception.DeniedEditTeamBuilding;
+import com.blackcompany.eeos.teamBuilding.persistence.TeamBuildingStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,6 +29,16 @@ public class TeamBuildingModel implements AbstractModel {
 	public void validateEdit(Long memberId) {
 		if (!this.memberId.equals(memberId)) {
 			throw new DeniedEditTeamBuilding();
+		}
+	}
+
+	public void validateAttend() {
+		validateStatus();
+	}
+
+	private void validateStatus() {
+		if (!TeamBuildingStatus.validateSame(status, TeamBuildingStatus.PROGRESS)) {
+			throw new DeniedSaveAttendTeamBuildingException();
 		}
 	}
 }
