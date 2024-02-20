cf788fcd8ab8b7a7d640af10bd7b51a79de9f830
kssumin <201566@jnu.ac.kr>
2024-02-21 07:53:57 +0900

    feat : 팀빌딩 결과를 담을 entity 구현

---
+++ BE/eeos/src/main/java/com/blackcompany/eeos/teamBuilding/persistence/TeamBuildingResultEntity.java
@@ -0,0 +1,47 @@
+package com.blackcompany.eeos.teamBuilding.persistence;
+
+import com.blackcompany.eeos.common.persistence.BaseEntity;
+import com.blackcompany.eeos.common.persistence.IntegerArrayConverter;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@SuperBuilder(toBuilder = true)
+@Entity
+@Table(name = TeamBuildingResultEntity.ENTITY_PREFIX)
+@SQLDelete(sql = "UPDATE team_building_result SET is_deleted=true where team_building_result_id=?")
+@Where(clause = "is_deleted=false")
+public class TeamBuildingResultEntity extends BaseEntity {
+	public static final String ENTITY_PREFIX = "team_building_result";
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = ENTITY_PREFIX + "_id", nullable = false)
+	private Long id;
+
+	@Column(name = ENTITY_PREFIX + "_member_ids", nullable = false)
+	@Convert(converter = IntegerArrayConverter.class)
+	private List<Long> memberIds;
+
+	@Column(name = ENTITY_PREFIX + "_status", nullable = false)
+	@Builder.Default
+	private TeamBuildingStatus status = TeamBuildingStatus.COMPLETE;
+}
---
+++ BE/eeos/src/main/java/com/blackcompany/eeos/teamBuilding/persistence/TeamBuildingResultRepository.java
@@ -0,0 +1,9 @@
+package com.blackcompany.eeos.teamBuilding.persistence;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamBuildingResultRepository
+		extends JpaRepository<TeamBuildingResultEntity, Long> {
+	List<TeamBuildingResultEntity> findTopByStatus(TeamBuildingStatus status);
+}
