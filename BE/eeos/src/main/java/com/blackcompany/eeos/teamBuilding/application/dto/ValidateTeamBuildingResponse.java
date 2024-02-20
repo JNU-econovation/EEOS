3985c1e4023bd243ea473fa1d57417914a444de6
kssumin <201566@jnu.ac.kr>
2024-02-20 12:51:25 +0900

    feat : 팀빌딩 상태 응답

---
+++ BE/eeos/src/main/java/com/blackcompany/eeos/teamBuilding/application/dto/ValidateTeamBuildingResponse.java
@@ -0,0 +1,15 @@
+package com.blackcompany.eeos.teamBuilding.application.dto;
+
+import com.blackcompany.eeos.common.support.dto.AbstractResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class ValidateTeamBuildingResponse implements AbstractResponseDto {
+	private String status;
+}
