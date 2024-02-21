068a091c90b54694c92d5efb04a217d6bdb18883
bada308 <203190@jnu.ac.kr>
2024-02-21 22:14:23 +0900

    feat: 팀빌딩 결과 조회 시 쿼리 적용

--- FE/src/components/teamBuildingResult/TeamResultInfo.container.tsx
+++ FE/src/components/teamBuildingResult/TeamResultInfo.container.tsx
@@ -1,15 +1,14 @@
+import { useGetTeamBuildingResultQuery } from "@/hooks/query/useTeamBuildingQuery";
 import TeamResultInfo from "./TeamResultInfo";
 
 const TeamResultInfoContainer = () => {
-  const teamResult = Array(5).fill(
-    Array(5).fill({
-      name: "25기 강바다",
-    }),
-  );
+  const { data: teamResult, isLoading } = useGetTeamBuildingResultQuery();
+
+  if (isLoading) return null;
 
   return (
     <div className="flex h-full w-full flex-col gap-16 overflow-y-scroll scrollbar-hide">
-      {teamResult.map((members, index) => (
+      {teamResult.result.map((members, index) => (
         <TeamResultInfo key={index} index={index} members={members} />
       ))}
     </div>
