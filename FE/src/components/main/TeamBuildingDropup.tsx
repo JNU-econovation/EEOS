d94f108177ac8fa5dc38cf673185d87f15a44d6f
bada308 <203190@jnu.ac.kr>
2024-02-21 22:10:13 +0900

    feat: 팀빌딩 생성하기 클릭, 팀빌딩 참여하기 클릭 시 로직 추가

--- FE/src/components/main/TeamBuildingDropup.tsx
+++ FE/src/components/main/TeamBuildingDropup.tsx
@@ -1,25 +1,50 @@
 import Image from "next/image";
 import Dropup from "../common/Dropup";
 import { useRouter } from "next/navigation";
+import ROUTES from "@/constants/ROUTES";
+import {
+  useGetIsCreableQuery,
+  useGetIsJoinableQuery,
+} from "@/hooks/query/useTeamBuildingQuery";
+import { toast } from "react-toastify";
+import ERROR_MESSAGE from "@/constants/ERROR_MESSAGE";
+import ERROR_CODE from "@/constants/ERROR_CODE";
 
 const TeamBuildingDropup = () => {
   const router = useRouter();
+  const { refetch: refetchCreatable } = useGetIsCreableQuery();
+  const { refetch: refetchJoinable } = useGetIsJoinableQuery();
 
   const TITLE = "AI 팀빌딩";
   const ITEMS = [
     {
       text: "팀빌딩 생성하기",
-      onClick: () => {
-        router.push("/team-building/create");
+      onClick: async () => {
+        const latestCreatable = (await refetchCreatable()).data;
+        if (latestCreatable.status === "nonCreatable") {
+          toast.error(
+            ERROR_MESSAGE[ERROR_CODE.TEAM_BUILDING.NOT_CREATABLE].message,
+          );
+          return;
+        }
+        router.push(ROUTES.TEAM_BUILDING.CREATE);
       },
     },
     {
       text: "팀빌딩 참여하기",
-      onClick: () => {
-        router.push("/team-building/detail");
+      onClick: async () => {
+        const latestJoinable = (await refetchJoinable()).data;
+        if (latestJoinable.status === "nonJoinable") {
+          toast.error(
+            ERROR_MESSAGE[ERROR_CODE.TEAM_BUILDING.NOT_JOINABLE].message,
+          );
+          return;
+        }
+        router.push(ROUTES.TEAM_BUILDING.DETAIL);
       },
     },
   ];
+
   return (
     <Dropup title={TITLE} items={ITEMS}>
       <Image src="/icons/robot.svg" alt="팀빌딩 로봇" width={28} height={28} />
