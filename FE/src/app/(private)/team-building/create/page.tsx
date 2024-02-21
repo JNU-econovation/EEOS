d8ac02af91eb3ff25d9f6d0305933b154fcd6f0e
bada308 <203190@jnu.ac.kr>
2024-02-21 23:07:29 +0900

    feat: 팀빌딩 생성 페이지 접근 시 생성 가능한지 확인

--- FE/src/app/(private)/team-building/create/page.tsx
+++ FE/src/app/(private)/team-building/create/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
 import Title from "@/components/common/Title";
 import TeamBuildingCreateForm from "@/components/teamBuildingCreate/TeamBuildingCreateForm";
+import ERROR_CODE from "@/constants/ERROR_CODE";
+import ERROR_MESSAGE from "@/constants/ERROR_MESSAGE";
+import ROUTES from "@/constants/ROUTES";
+import { useGetIsCreableQuery } from "@/hooks/query/useTeamBuildingQuery";
+import { useRouter } from "next/navigation";
+
+import { toast } from "react-toastify";
 
 const TeamBuildingCreatePage = () => {
+  const router = useRouter();
+  const { data: creatable, isLoading } = useGetIsCreableQuery();
+
+  isLoading && <div>로딩중...</div>;
+
+  if (!creatable) {
+    toast.error(ERROR_MESSAGE[ERROR_CODE.TEAM_BUILDING.NOT_CREATABLE].message);
+    router.push(ROUTES.MAIN);
+  }
+
   return (
     <>
       <div className="space-y-12">
