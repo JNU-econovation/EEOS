081e378d41de893cc452aba0b4dff90f8fff08c8
kssumin <201566@jnu.ac.kr>
2024-02-22 15:45:49 +0900

    feat : 팀빌딩 종료 예외 구현

---
+++ BE/eeos/src/main/java/com/blackcompany/eeos/teamBuilding/application/exception/EndTeamBuildingException.java
@@ -0,0 +1,17 @@
+package com.blackcompany.eeos.teamBuilding.application.exception;
+
+import com.blackcompany.eeos.common.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+
+public class EndTeamBuildingException extends BusinessException {
+	private static final String FAIL_CODE = "6011";
+
+	public EndTeamBuildingException() {
+		super(FAIL_CODE, HttpStatus.BAD_REQUEST);
+	}
+
+	@Override
+	public String getMessage() {
+		return "팀빌딩이 종료되어 해당 팀빌딩을 조회할 수 없습니다.";
+	}
+}
