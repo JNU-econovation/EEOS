e63722c82d3e6edb0cc7422faa525042ffe2372c
kssumin <201566@jnu.ac.kr>
2024-03-09 13:48:20 +0900

    chore : 사용하지 않는 import문 제거

--- BE/eeos/src/main/java/com/blackcompany/eeos/auth/application/service/ReissueService.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/auth/application/service/ReissueService.java
@@ -7,14 +7,12 @@ import com.blackcompany.eeos.auth.application.support.AuthenticationTokenGenerat
 import com.blackcompany.eeos.auth.application.usecase.ReissueUsecase;
 import com.blackcompany.eeos.auth.persistence.MemberAuthenticationRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-@Slf4j
 public class ReissueService implements ReissueUsecase {
 	private final AuthenticationTokenGenerator authenticationTokenGenerator;
 	private final MemberAuthenticationRepository memberAuthenticationRepository;
