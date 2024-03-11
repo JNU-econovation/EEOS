d424a933dc7d3b077927afb9bd3f56201c42ecda
kssumin <201566@jnu.ac.kr>
2024-03-11 21:14:56 +0900

    refactor : 이미 탈퇴 처리 된 회원이면 에러 발생하지 않고 return 되도록 변경

--- BE/eeos/src/main/java/com/blackcompany/eeos/auth/application/service/DeactivateMemberService.java
+++ BE/eeos/src/main/java/com/blackcompany/eeos/auth/application/service/DeactivateMemberService.java
@@ -5,6 +5,8 @@ import com.blackcompany.eeos.auth.application.event.DeletedMemberEvent;
 import com.blackcompany.eeos.auth.application.usecase.LogOutUsecase;
 import com.blackcompany.eeos.auth.application.usecase.WithDrawUsecase;
 import com.blackcompany.eeos.auth.persistence.BlackAuthenticationRepository;
+import com.blackcompany.eeos.auth.persistence.OAuthMemberRepository;
+import com.blackcompany.eeos.member.persistence.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -13,10 +15,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class DeactivationMemberService implements LogOutUsecase, WithDrawUsecase {
+public class DeactivateMemberService implements LogOutUsecase, WithDrawUsecase {
 	private final BlackAuthenticationRepository blackAuthenticationRepository;
 	private final TokenResolver tokenResolver;
 	private final ApplicationEventPublisher eventPublisher;
+	private final MemberRepository memberRepository;
+	private final OAuthMemberRepository oAuthMemberRepository;
 
 	@Override
 	@Transactional
@@ -27,8 +31,11 @@ public class DeactivationMemberService implements LogOutUsecase, WithDrawUsecase
 	@Override
 	@Transactional
 	public void withDraw(final String token, final Long memberId) {
-		saveUsedToken(token, memberId);
-		eventPublisher.publishEvent(DeletedMemberEvent.of(memberId));
+		if (isNotMember(memberId)) {
+			return;
+		}
+		deleteMemberData(memberId);
+		eventPublisher.publishEvent(DeletedMemberEvent.of(memberId, token));
 	}
 
 	private void saveUsedToken(final String token, final Long memberId) {
@@ -37,5 +44,14 @@ public class DeactivationMemberService implements LogOutUsecase, WithDrawUsecase
 
 	private Long getExpiredToken(final String token) {
 		return tokenResolver.getExpiredDateByRefreshToken(token);
+	}
+
+	private boolean isNotMember(Long memberId) {
+		return memberRepository.findById(memberId).isEmpty();
+	}
+
+	private void deleteMemberData(Long memberId) {
+		memberRepository.deleteById(memberId);
+		oAuthMemberRepository.findByMemberId(memberId).ifPresent(oAuthMemberRepository::delete);
 	}
 }
